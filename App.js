754119579973deafda29cb1bd4d75c618031e913
Hanna <hannaco@ucla.edu>
2020-07-11 11:59:10 -0700

    fixed API call
    changed the array used and fixed useEffects

--- App.js
+++ App.js
@@ -9,9 +9,9 @@
 import React, {Component, useState, useEffect} from 'react';
 import YelpApi from './api.js';
 import Swiper from 'react-native-deck-swiper';
-import { Transitioning, Transition } from 'react-native-reanimated';
+import {Transitioning, Transition} from 'react-native-reanimated';
 import data from './data';
-import{facebookService} from './facebookService.js';
+import {facebookService} from './facebookService.js';
 import {
   Image,
   SafeAreaView,
@@ -20,12 +20,13 @@ import {
   View,
   Text,
   StatusBar,
-  Dimensions
+  Dimensions,
 } from 'react-native';
 
 export default function App() {
   const [index, setIndex] = useState(0);
   const [results, setResults] = useState([]);
+  const round = 1;
 
   useEffect(() => {
     YelpApi.getRestaurants('boba', 'arcadia california').then(response => {
@@ -33,10 +34,10 @@ export default function App() {
         console.log(response.total),
         setResults(response.businessList);
     });
-  });
+  }, []);
 
   const Card = ({card}) => {
-    while (results.length == 0)
+    while (results.length === 0)
       return <Text style={styles.card}>Fetching Restaurants!</Text>;
     return (
       <View style={styles.card}>
@@ -67,106 +68,102 @@ export default function App() {
   };
   return (
     <SafeAreaView style={styles.mainContainer}>
-    <View style={styles.cardContainer}>
-      <Swiper
-        cards = {data}
-        cardIndex= {index}
-        renderCard= {(card) => <Card card= {card}/>}
-        onSwiper = {onSwiped}
-        stackSize = {10}
-        stackSeparation = {0}
-        backgroundColor= {'transparent'}
-
-        animateOverlayLabelsOpacity
-
-        //Overlay offsets adjusted to flex sizing. May need to be retested on different device
-        overlayLabels={{
-          left: {
-            title: 'NOPE',
-            style: {
-              label: {
-                backgroundColor: 'red',
-                borderColor: 'red',
-                color: 'white',
-                borderWidth: 1,
-                fontSize: 24
+      <View style={styles.cardContainer}>
+        <Swiper
+          cards={results}
+          cardIndex={index}
+          renderCard={card => <Card card={card} />}
+          onSwiper={onSwiped}
+          stackSize={10}
+          stackSeparation={0}
+          backgroundColor={'transparent'}
+          animateOverlayLabelsOpacity
+          //Overlay offsets adjusted to flex sizing. May need to be retested on different device
+          overlayLabels={{
+            left: {
+              title: 'NOPE',
+              style: {
+                label: {
+                  backgroundColor: 'red',
+                  borderColor: 'red',
+                  color: 'white',
+                  borderWidth: 1,
+                  fontSize: 24,
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-start',
+                  marginTop: 20,
+                  marginLeft: -50,
+                },
               },
-              wrapper: {
-                flexDirection: 'column',
-                alignItems: 'flex-end',
-                justifyContent: 'flex-start',
-                marginTop: 20,
-                marginLeft: -50
-              }
-            }
-          },
-          right: {
-            title: 'LIKE',
-            style: {
-              label: {
-                backgroundColor: 'green',
-                borderColor: 'green',
-                color: 'white',
-                borderWidth: 1,
-                fontSize: 24
+            },
+            right: {
+              title: 'LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'green',
+                  borderColor: 'green',
+                  color: 'white',
+                  borderWidth: 1,
+                  fontSize: 24,
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  marginTop: 20,
+                  marginLeft: 20,
+                },
               },
-              wrapper: {
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                justifyContent: 'flex-start',
-                marginTop: 20,
-                marginLeft: 20
-              }
-            }
-          },
-          bottom: {
-            title: 'HATE',
-            style: {
-              label: {
-                backgroundColor: 'black',
-                borderColor: 'black',
-                color: 'white',
-                borderWidth: 1,
-                fontSize: 24
+            },
+            bottom: {
+              title: 'HATE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1,
+                  fontSize: 24,
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'flex-start',
+                  marginTop: 20,
+                  marginLeft: -20,
+                },
               },
-              wrapper: {
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'flex-start',
-                marginTop: 20,
-                marginLeft: -20
-              }
-            }
-          },
-          top: {
-            title: 'LOVE',
-            style: {
-              label: {
-                backgroundColor: 'pink',
-                borderColor: 'pink',
-                color: 'white',
-                borderWidth: 1,
-                fontSize: 24
+            },
+            top: {
+              title: 'LOVE',
+              style: {
+                label: {
+                  backgroundColor: 'pink',
+                  borderColor: 'pink',
+                  color: 'white',
+                  borderWidth: 1,
+                  fontSize: 24,
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'flex-end',
+                  marginTop: -50,
+                  marginLeft: -20,
+                },
               },
-              wrapper: {
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'flex-end',
-                marginTop: -50,
-                marginLeft: -20
-              }
-            }
-          }
-
-        }}
-      />
-    </View>
+            },
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 }
- 
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
   //Fullscreen
   mainContainer: {
     flex: 1,
@@ -180,8 +177,8 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     alignSelf: 'center',
     marginTop: 5,
-    width: "90%",
-    aspectRatio: 5/8,
+    width: '90%',
+    aspectRatio: 5 / 8,
   },
 
   //Sizing is now based on aspect ratio
@@ -191,8 +188,8 @@ const styles = StyleSheet.create({
     borderWidth: 0,
     borderColor: '#000',
     alignSelf: 'flex-start',
-    width: "90%",
-    aspectRatio: 5/8,
+    width: '90%',
+    aspectRatio: 5 / 8,
   },
 
   //Card image is now centered
@@ -202,4 +199,3 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
 });
-
