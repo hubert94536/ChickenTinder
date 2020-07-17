8a70966297fdaf96cf60c1eeb28602d7120b4207
Hubert Chen <hubert94536@yahoo.com>
2020-07-16 21:27:42 -0700

    heroku

--- backend/accountsServer.js
+++ backend/accountsServer.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 //const db = require('./queries.js')
-const port = process.env.PORT || 5000
+const port = process.env.PORT 
 
 app.use(bodyParser.json())
 app.use(
