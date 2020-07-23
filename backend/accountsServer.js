e97d286190732eeeb81038f06366505f894b49bf
Hubert Chen <hubert94536@yahoo.com>
2020-07-22 17:54:22 -0700

    port changes

--- backend/accountsServer.js
+++ backend/accountsServer.js
@@ -15,7 +15,7 @@ app.use(
 // app.get('/', (request, response) => {
 //   response.json({ info: 'Node.js, Express, and Postgres API' })
 // })
-app.get('/accounts', db.getUsers)
+//app.get('/accounts', db.getUsers)
 //app.get('/accounts/:id', db.getUserById)
 // app.post('/users', db.createUser)
 // app.put('/users/:id', db.updateUser)
