9dbf7edcd3cb58a64dd2f492510f9726b32bdbd0
Hanna <hannaco@ucla.edu>
2020-07-26 23:47:05 -0700

    Geolocation
    added geolocation to api.js

--- api.js
+++ api.js
@@ -1,5 +1,43 @@
-import axios from 'axios'
-import {API_KEY} from 'react-native-dotenv'
+import axios from 'axios';
+import {API_KEY} from 'react-native-dotenv';
+import PermissionsAndroid from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
+// import Geolocation from '@react-native-community/geolocation';
+
+// const App = () => {
+//   Geolocation.getCurrentPosition(data => console.warn(data));
+//   console.warn('Geolocation');
+//   return (
+//     <View>
+//       <Text style={{fontSize: 70}}>latitude longitude</Text>
+//     </View>
+//   );
+// };
+
+// const requestLocationPermission = async () => {
+//   try {
+//     const granted = await PermissionsAndroid.request(
+//       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+//       {
+//         title: 'Location Permission',
+//         message:
+//           'WeChews needs access to your location ' +
+//           'so you can find nearby restaurants.',
+//         buttonNeutral: 'Ask Me Later',
+//         buttonNegative: 'Cancel',
+//         buttonPositive: 'OK',
+//       },
+//     );
+//     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+//       console.log('Location access granted');
+//     } else {
+//       console.log('Location access denied');
+//     }
+//   } catch (err) {
+//     console.warn(err);
+//   }
+// };
 
 //setting up Yelp API base caller
 const api = axios.create({
@@ -7,38 +45,40 @@ const api = axios.create({
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
-})
+});
 
 //getting list of resturants
-const getRestaurants = (name, place)  => {
-  return api
-    .get('/businesses/search', {
-      params: {
-        term: name,
-        location: place,
-      },
-    })
-    //returns business info from Yelp
-    .then(res => {
+const getRestaurants = (name, place) => {
+  return (
+    api
+      .get('/businesses/search', {
+        params: {
+          term: name,
+          location: place,
+        },
+      })
+      //returns business info from Yelp
+      .then(res => {
         return {
-            total: res.data.total,
-            businessList: res.data.businesses.map(function (business) { 
-                return {
-                    name: business.name,
-                    distance: business.distance,
-                    categories: business.categories,
-                    reviewCount: business.review_count,
-                    rating: business.rating,
-                    price: business.price,
-                    phone: business.display_phone,
-                    location: business.location,
-                    isClosed: business.is_closed
-                }
-            })
-        }
-    })
-    .catch(error => console.error(error))
-}
+          total: res.data.total,
+          businessList: res.data.businesses.map(function(business) {
+            return {
+              name: business.name,
+              distance: business.distance,
+              categories: business.categories,
+              reviewCount: business.review_count,
+              rating: business.rating,
+              price: business.price,
+              phone: business.display_phone,
+              location: business.location,
+              isClosed: business.is_closed,
+            };
+          }),
+        };
+      })
+      .catch(error => console.error(error))
+  );
+};
 
 export default {
   getRestaurants,
