808b89ef2d1ca6a562ea3653c1820c28d535f738
Hubert Chen <hubert94536@yahoo.com>
2020-08-03 13:30:42 -0700

    remove sync

--- backend/accountsModel.js
+++ backend/accountsModel.js
@@ -59,8 +59,8 @@ const Accounts = sequelize.define('accounts', {
     photo: DataTypes.STRING,
 });
 
-Accounts.sync({ force: true }).then(()=> {
-    console.log("All models were synchronized successfully.");
-})
+// Accounts.sync({ force: true }).then(()=> {
+//     console.log("All models were synchronized successfully.");
+// })
 
 module.exports = Accounts;
