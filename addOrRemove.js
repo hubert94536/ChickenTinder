c2a4abac391010e6472dc62cf5d4425c1f1d7896
Isha G <igonugun@g.ucla.edu>
2020-08-08 10:57:33 -0700

    filters stage 1

---
+++ addOrRemove.js
@@ -0,0 +1,10 @@
+export const addOrRemove = (array, item) => {
+    const exists = array.includes(item)
+  if (exists) {
+      return array.filter((c) => { return c !== item })
+    } else {
+      const result = array
+      result.push(item)
+      return result
+    }
+  }
