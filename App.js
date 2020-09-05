1aa19247bd3800a7b493465aa1d6bd734e0ca988
Isha G <igonugun@g.ucla.edu>
2020-09-05 13:19:10 -0700

    filter updated

--- App.js
+++ App.js
@@ -1,232 +1,259 @@
-/*
-    Filter Selection Menu
-*/
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
 
-import React from 'react';
+// import React, { Component, useState, useEffect } from 'react';
+// import api from './api.js';
+// import Swiper from 'react-native-deck-swiper';
+// import { Transitioning, Transition } from 'react-native-reanimated';
+// import data from './data';
+// import { facebookService } from './facebookService.js';
+// import {
+//   Image,
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+//   Dimensions,
+// } from 'react-native';
+
+// export default function App() {
+//   const [index, setIndex] = useState(0);
+//   const [results, setResults] = useState([]);
+
+//   useEffect(() => {
+//     api.getRestaurants('boba', 'arcadia california').then(response => {
+//       console.log(response.businessList),
+//         console.log(response.total),
+//         setResults(response.businessList);
+//         api.checkUsername('hub');
+//     });
+//   }, []);
+
+//   const Card = ({card}) => {
+//     while (results.length == 0)
+//       return <Text style={styles.card}>Fetching Restaurants!</Text>;
+//     return (
+//       <View style={styles.card}>
+//         {/* <Image source={{uri: card.image}} style={styles.cardImage} /> */}
+//         <Text>
+//           {card.price} {card.name}
+//         </Text>
+//         <Text>{card.reviewCount} Reviews</Text>
+//         <Text>Average Rating: {card.rating} Stars</Text>
+//         <Text style={{fontWeight: 'bold'}}>
+//           Contact Information:{' '}
+//           <Text style={{fontWeight: 'normal'}}>{card.phone}</Text>
+//         </Text>
+//         <Text style={{fontWeight: 'bold'}}>
+//           Address:{' '}
+//           <Text style={{fontWeight: 'normal'}}>
+//             {/* {card.location.display_address[0]},{' '}
+//             {card.location.display_address[1]} */}
+//           </Text>
+//         </Text>
+//       </View>
+//     );
+//   };
+
+//   const onSwiped = () => {
+//     // transitionRef.current.animateNextTransition();
+//     setIndex(index + 1);
+//   };
+//   return (
+//     <SafeAreaView style={styles.mainContainer}>
+//     <View style={styles.cardContainer}>
+//       <Swiper
+//         cards = {data}
+//         cardIndex= {index}
+//         renderCard= {(card) => <Card card= {card}/>}
+//         onSwiper = {onSwiped}
+//         stackSize = {10}
+//         stackSeparation = {0}
+//         backgroundColor= {'transparent'}
+
+//         animateOverlayLabelsOpacity
+
+//         //Overlay offsets adjusted to flex sizing. May need to be retested on different device
+//         overlayLabels={{
+//           left: {
+//             title: 'NOPE',
+//             style: {
+//               label: {
+//                 backgroundColor: 'red',
+//                 borderColor: 'red',
+//                 color: 'white',
+//                 borderWidth: 1,
+//                 fontSize: 24
+//               },
+//               wrapper: {
+//                 flexDirection: 'column',
+//                 alignItems: 'flex-end',
+//                 justifyContent: 'flex-start',
+//                 marginTop: 20,
+//                 marginLeft: -50
+//               }
+//             }
+//           },
+//           right: {
+//             title: 'LIKE',
+//             style: {
+//               label: {
+//                 backgroundColor: 'green',
+//                 borderColor: 'green',
+//                 color: 'white',
+//                 borderWidth: 1,
+//                 fontSize: 24
+//               },
+//               wrapper: {
+//                 flexDirection: 'column',
+//                 alignItems: 'flex-start',
+//                 justifyContent: 'flex-start',
+//                 marginTop: 20,
+//                 marginLeft: 20
+//               }
+//             }
+//           },
+//           bottom: {
+//             title: 'HATE',
+//             style: {
+//               label: {
+//                 backgroundColor: 'black',
+//                 borderColor: 'black',
+//                 color: 'white',
+//                 borderWidth: 1,
+//                 fontSize: 24
+//               },
+//               wrapper: {
+//                 flexDirection: 'column',
+//                 alignItems: 'center',
+//                 justifyContent: 'flex-start',
+//                 marginTop: 20,
+//                 marginLeft: -20
+//               }
+//             }
+//           },
+//           top: {
+//             title: 'LOVE',
+//             style: {
+//               label: {
+//                 backgroundColor: 'pink',
+//                 borderColor: 'pink',
+//                 color: 'white',
+//                 borderWidth: 1,
+//                 fontSize: 24
+//               },
+//               wrapper: {
+//                 flexDirection: 'column',
+//                 alignItems: 'center',
+//                 justifyContent: 'flex-end',
+//                 marginTop: -50,
+//                 marginLeft: -20
+//               }
+//             }
+//           }
+
+//         }}
+//       />
+//     </View>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+
+//   //Fullscreen
+//   mainContainer: {
+//     flex: 1,
+//     backgroundColor: '#6495ed',
+//   },
+
+//   //Card area is now flexsized and takes 90% of the width of screen
+//   cardContainer: {
+//     borderRadius: 17,
+//     borderWidth: 0,
+//     borderColor: '#000',
+//     alignSelf: 'center',
+//     marginTop: 5,
+//     width: "90%",
+//     aspectRatio: 5/8,
+//   },
+
+//   //Sizing is now based on aspect ratio
+//   card: {
+//     backgroundColor: '#fff',
+//     borderRadius: 17,
+//     borderWidth: 0,
+//     borderColor: '#000',
+//     alignSelf: 'flex-start',
+//     width: "90%",
+//     aspectRatio: 5/8,
+//   },
+
+//   //Card image is now centered
+//   cardImage: {
+//     marginTop: 60,
+//     flex: 0.5,
+//     resizeMode: 'contain',
+//   },
+// });
+
+import React, {useState} from 'react';
+import {Button, TextInput} from 'react-native';
+import firebase from 'firebase';
 import {
-    Image,
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    StatusBar,
-    Dimensions,
-    Button,
-    TouchableOpacity
-  } from 'react-native';
-import TagsView from './TagsView';
-import Slider from '@react-native-community/slider';
-import BackgroundButton from './BackgroundButton';
-
-const SCREEN_WIDTH = Dimensions.get('window').width;
-
-const selectedCuisine = []
-const tagsCuisine = ['Japanese', 'Korean', 'Chinese', 'Thai', 'Mongolian', 'Taiwanese', 'Indian', 'Vietnamese']
-
-const selectedDining = []
-const tagsDining = ['Dine-in', 'Delivery', 'Catering', 'Pickup']
-
-const selectedPrice = []
-const tagsPrice = ['$', '$$', '$$$', '$$$$', 'Any']
-
-const selectedDiet = []
-const tagsDiet = ['Eggs', 'Nuts', 'Fish', 'Pork', 'Dairy', 'Chicken', 'Beef', 'Sesame', 'Gluten', 'Soy']
-
-const mainColor = "#DE4A4A"
-
-
-export default function filterSelector(){
-
+  FIREBASE_API_KEY,
+  FIREBASE_APPLICATION_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_PROJECT_ID,
+} from 'react-native-dotenv';
+const config = {
+  apiKey: FIREBASE_API_KEY, // Auth / General Use
+  applicationId: FIREBASE_APPLICATION_ID, // General Use
+  projectId: FIREBASE_PROJECT_ID, // General Use
+  authDomain: FIREBASE_AUTH_DOMAIN, // Auth with popup/redirect
+  databaseURL: FIREBASE_DATABASE, // Realtime Database
+  storageBucket: FIREBASE_STORAGE_BUCKET, //Storage
+};
+firebase.initializeApp(config);
+export default function PhoneSignIn() {
+  // If null, no SMS has been sent
+  const [confirm, setConfirm] = useState(null);
+  const [code, setCode] = useState('');
+  // Handle the button press
+  async function signInWithPhoneNumber(phoneNumber) {
+    const confirmation = await firebase
+      .auth()
+      .signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+    } catch (error) {
+      console.log('Invalid code.');
+    }
+  }
+  if (!confirm) {
     return (
-        <SafeAreaView style={styles.mainContainer }>
-        <View style={styles.cardContainer}>
-
-          <Text style ={{fontFamily: 'CircularStd-Bold', fontSize: 28, color: 'white', paddingBottom:10, paddingLeft: SCREEN_WIDTH * 0.045}}> Set Your Filters</Text>
-          <View style={styles.card}>
-            <View style={styles.section}>
-              <Text style={styles.header}>Cuisines</Text>
-              <TagsView
-                all={tagsCuisine}
-                selected={selectedCuisine}
-                isExclusive={false}
-              />
-            </View>
-
-            <View style={[styles.section, styles.section2]}>
-              <Text style={styles.header}>Distance</Text>
-              <Slider
-                style={{width: SCREEN_WIDTH*0.75, height: 30, alignSelf: 'center'}}
-                minimumValue={5}
-                maximumValue={25}
-                value = {5}
-                minimumTrackTintColor = "#DE4A4A"
-                maximumTrackTintColor='#000000'
-                thumbTintColor= "#DE4A4A"
-            />  
-            </View>
-
-            <View style={[styles.section, styles.section2]}>
-              <Text style={styles.header}>Dining Options</Text>
-              <TagsView
-                all={tagsDining}
-                selected={selectedDining}
-                isExclusive={false}
-              />
-            </View>
-
-            <View style={[styles.section, styles.section2]}>
-              <Text style={styles.header}>Price</Text>
-              <TagsView
-                all={tagsPrice}
-                selected={selectedPrice}
-                isExclusive={false}
-              />
-            </View>
-
-            <View style={[styles.section]}>
-              <Text style={styles.header}>Dietary Restrictions</Text>
-              <TagsView
-                all={tagsDiet}
-                selected={selectedDiet}
-                isExclusive={false}
-              />
-            </View>
-
-            {/* <Button 
-              title = "Let's Go"
-              color = 'white'
-              fontSize = 20;
-            
-            />
-               */}
-
-            {/* <TouchableOpacity style={styles.touchable}>
-              <View style={styles.view}>
-                <Text style={styles.text}>Let's Go</Text>
-              </View>
-            </TouchableOpacity> */}
-          
-          
-
-          </View>
-        </View>
-
-        <View style = {{paddingTop: 10, width: 150, alignSelf: 'center'}}>
-        {/* <BackgroundButton
-          backgroundColor= 'white'
-          textColor= 'pink'
-          borderColor= 'transparent'
-          // onPress={() => {
-          //   this.onPress(tag)
-          // }}
-          title = "Let's Go" /> */}
-
-          <TouchableOpacity style={styles.touchable}>
-            <View style={styles.nextButton}>
-            <Text style={styles.nextTitle}>Let's Go</Text>
-            </View>
-          </TouchableOpacity>
-
-          </View>
-
-
-        </SafeAreaView>
-      )
-  
+      <Button
+        title="Phone Number Sign In"
+        onPress={() => signInWithPhoneNumber('+1 909-532-4384')}
+      />
+    );
+  }
+  return (
+    <>
+      <TextInput value={code} onChangeText={text => setCode(text)} />
+      <Button title="Confirm Code" onPress={() => confirmCode()} />
+    </>
+  );
 }
-
-
-
-
-
-const styles = StyleSheet.create({
-  //Fullscreen
-  mainContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: mainColor,
-  },
-  //Card area is now flexsized and takes 90% of the width of screen
-  cardContainer: {
-   // backgroundColor: '#ff0',
-    borderRadius: 17,
-    borderWidth: 0,
-    borderColor: '#000',
-    justifyContent: 'flex-end',
-    alignSelf: 'center',
-    marginTop: 5,
-    width: "90%",
-    aspectRatio: 5/8,
-  },
-  //Sizing is now based on aspect ratio
-  card: {
-    backgroundColor: '#fff',
-    borderRadius: 17,
-    borderWidth: 0,
-    borderColor: '#000',
-    alignSelf: 'center',
-    width: "90%",
-    aspectRatio: 5/8,
-    // justifyContent: 'center'
-  },
-  section: {
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderColor: 'transparent',
-    alignSelf: 'center',
-    width: "100%",
-    height:"27.5%",
-    paddingHorizontal:10,
-    paddingVertical:7.5,
-    borderBottomColor: mainColor
-  },
-
-  section2: {
-    height:"15%",
-  },
-
-  header: {
-    textAlign: 'left',
-    color: mainColor,
-    fontSize: 18,
-    fontFamily: 'CircularStd-Bold',
-    // paddingTop: 10,
-  },
-
-  touchable: {
-    // marginLeft: 4,
-    marginRight: 8,
-    marginBottom: 6
-
-    // marginLeft: 3,
-    // marginRight: 3,
-    // marginBottom: 6
-  },
-
-  nextTitle: {
-    // fontSize: 5,
-    textAlign: 'center',
-    color: 'white',
-    // fontSize: 16
-    fontSize: 18,
-    fontFamily: 'CircularStd-Bold',
-  },
-
-  nextButton: {
-    // flexDirection: 'row',
-    // borderRadius: 23,
-    borderColor: 'white',
-    borderWidth: 2,
-    borderRadius: 18,
-    backgroundColor: mainColor,
-    // height: 46,
-    height: 36,
-    alignItems: 'center',
-    justifyContent: 'center',
-    // paddingLeft: 16,
-    // paddingRight: 16
-
-    paddingHorizontal: 8
-  },
-});
