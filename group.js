670e84e2dba74d05036398dba8954f6b12862a8c
Hanna <hannaco@ucla.edu>
2020-09-06 14:20:45 -0700

    Group Page
    fixed the render to take in parameters

--- group.js
+++ group.js
@@ -11,15 +11,62 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 import UserProfileView from './profile.js';
 
+const hex = '#F25763';
+
+var participants = [
+  {
+    name: 'Hanna',
+    username: '@hannaco',
+    image:
+      'https://d1kdq4z3qhht46.cloudfront.net/uploads/2019/08/Adventures_from_Moominvalley_1990_Moomintroll_TV.jpg',
+  },
+  {
+    name: 'Isha',
+    username: '@ishagonu',
+    image:
+      'https://www.abramsandchronicle.co.uk/wp-content/uploads/books/9781452182674.jpg',
+  },
+  {
+    name: 'Hubert',
+    username: '@hubesc',
+    image: 'https://media0.giphy.com/media/ayMW3eqvuP00o/giphy_s.gif',
+  },
+  {
+    name: 'Ruth',
+    username: '@ruthlee',
+    image:
+      'https://wi-images.condecdn.net/image/baeWXm8eqMl/crop/405/f/ponyo.jpg',
+  },
+];
+
 export default class Group extends React.Component {
   render() {
+    var members = [];
+    var i = 0;
+    for (i = 0; i < participants.length; i++) {
+      members.push(
+        <Card
+          name={participants[i].name}
+          username={participants[i].username}
+          image={participants[i].image}
+        />,
+      );
+    }
+
     return (
       <View style={styles.main}>
         <View style={styles.top}>
           <Text style={styles.groupTitle}>Hubert's Group</Text>
           <View style={{flexDirection: 'row'}}>
             <Icon name="user" style={styles.icon} />
-            <Text style={{color: '#fff', fontWeight: 'bold'}}>4</Text>
+            <Text
+              style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                fontFamily: 'CircularStd-Medium',
+              }}>
+              {members.length}
+            </Text>
             <Text style={styles.divider}>|</Text>
             <Text style={styles.waiting}>waiting for 1 member's filters</Text>
           </View>
@@ -27,15 +74,7 @@ export default class Group extends React.Component {
             <Text style={styles.buttonText}>Set your filters</Text>
           </TouchableHighlight>
         </View>
-        <ScrollView style={styles.center}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </ScrollView>
+        <ScrollView style={styles.center}>{members}</ScrollView>
         <View style={styles.bottom}>
           <Text style={styles.bottomText}>
             When everyone has submitted filters, the round will begin!
@@ -53,16 +92,12 @@ class Card extends React.Component {
   render() {
     return (
       <View>
-        {/* <Text style={styles.card}>this is the card</Text> */}
         <View style={styles.card}>
-          <Image
-            source={{uri: 'https://www.dintaifungusa.com/image/1509'}}
-            style={styles.image}
-          />
+          <Image source={{uri: this.props.image}} style={styles.image} />
           <Icon
             name="check-circle"
             style={{
-              color: '#FF465E',
+              color: hex,
               fontSize: 20,
               position: 'absolute',
               marginLeft: '14%',
@@ -73,36 +108,41 @@ class Card extends React.Component {
             style={{
               alignSelf: 'center',
               marginLeft: '3%',
+              flex: 1,
             }}>
             <Text
               style={{
-                color: '#FF465E',
+                color: hex,
                 fontWeight: 'bold',
+                fontFamily: 'CircularStd-Medium',
               }}>
-              Hubert Chen
+              {this.props.name}
             </Text>
             <Text
               style={{
-                color: '#FF465E',
+                color: hex,
+                fontFamily: 'CircularStd-Medium',
               }}>
-              @hubesc
+              {this.props.username}
             </Text>
           </View>
-          <View style={{flexDirection: 'row', marginLeft: '30%'}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
             <Text
               style={{
-                color: '#FF465E',
+                color: hex,
                 alignSelf: 'center',
+                fontFamily: 'CircularStd-Medium',
+                marginLeft: '30%',
               }}>
               Remove
             </Text>
             <Icon
               name="times-circle"
               style={{
-                color: '#FF465E',
+                color: hex,
                 fontSize: 35,
                 alignSelf: 'center',
-                marginLeft: '10%',
+                marginLeft: '5%',
               }}
             />
           </View>
@@ -126,6 +166,7 @@ const styles = StyleSheet.create({
     marginLeft: '5%',
     marginTop: '5%',
     fontWeight: 'bold',
+    fontFamily: 'CircularStd-Medium',
   },
   icon: {
     color: '#fff',
@@ -138,8 +179,14 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginLeft: '3%',
     fontSize: 25,
+    fontFamily: 'CircularStd-Medium',
   },
-  waiting: {color: '#fff', marginLeft: '3%', alignSelf: 'center'},
+  waiting: {
+    color: '#fff',
+    marginLeft: '3%',
+    alignSelf: 'center',
+    fontFamily: 'CircularStd-Medium',
+  },
   button: {
     borderRadius: 25,
     borderWidth: 2.5,
@@ -155,6 +202,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     fontSize: 20,
     fontWeight: 'bold',
+    fontFamily: 'CircularStd-Medium',
   },
   bottomText: {
     color: '#fff',
@@ -163,6 +211,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: '3%',
     textAlign: 'center',
+    fontFamily: 'CircularStd-Medium',
   },
   bottomButton: {
     borderRadius: 30,
@@ -207,5 +256,10 @@ const styles = StyleSheet.create({
     marginTop: '3%',
     flexDirection: 'row',
   },
-  join: {marginTop: 0, marginLeft: '3%', color: '#fff'},
+  join: {
+    marginTop: 0,
+    marginLeft: '3%',
+    color: '#fff',
+    fontFamily: 'CircularStd-Medium',
+  },
 });
