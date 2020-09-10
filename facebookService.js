3990358b362f05283125375150d497d05cc3c9b9
Hubert Chen <hubert94536@yahoo.com>
2020-09-10 00:01:15 -0700

    fixed login flow

--- facebookService.js
+++ facebookService.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import FBSDK from 'react-native-fbsdk';
 import firebase from 'firebase';
 import api from './api.js';
@@ -17,7 +16,7 @@ import {
   PHOTO,
 } from 'react-native-dotenv';
 import AsyncStorage from '@react-native-community/async-storage';
-const {LoginManager, AccessToken, GraphRequest, GraphRequestManager} = FBSDK;
+const { LoginManager, AccessToken, GraphRequest, GraphRequestManager } = FBSDK;
 
 const config = {
   apiKey: FIREBASE_API_KEY, // Auth / General Use
@@ -32,68 +31,46 @@ firebase.initializeApp(config);
 
 class FacebookService {
   loginWithFacebook = () => {
-    console.log('logging in!');
     // Attempt a login using the Facebook login dialog asking for default permissions.
-    const result = LoginManager.logInWithPermissions([
+     return LoginManager.logInWithPermissions([
       'public_profile',
       'email',
     ])
       .then(login => {
         if (login.isCancelled) {
-          console.log('cancelled');
           return Promise.reject(new Error('Cancelled request'));
         }
-        console.log('access token');
         return AccessToken.getCurrentAccessToken();
       })
       .then(data => {
         const credential = firebase.auth.FacebookAuthProvider.credential(
           data.accessToken,
         );
-        console.log('credential');
         return firebase.auth().signInWithCredential(credential);
       })
       .then(currentUser => {
-        let p;
-        console.log('new user');
         if (currentUser.additionalUserInfo.isNewUser) {
           AsyncStorage.setItem(UID, firebase.auth().currentUser.uid);
-          AsyncStorage.setItem(
-            NAME,
+          AsyncStorage.setItem(NAME,
             currentUser.additionalUserInfo.profile.name,
           );
           AsyncStorage.setItem(ID, currentUser.additionalUserInfo.profile.id);
-          AsyncStorage.setItem(
-            EMAIL,
+          AsyncStorage.setItem(EMAIL,
             currentUser.additionalUserInfo.profile.email,
           );
           AsyncStorage.setItem(PHOTO, currentUser.user.photoURL);
-
           return 'Username';
-          //uncomment below code after finishing phone authentication
-          // api.createFBUser(
-          //   currentUser.additionalUserInfo.profile.name,
-          //   currentUser.additionalUserInfo.profile.id,
-          //   username,
-          //   currentUser.additionalUserInfo.profile.email,
-          //   currentUser.user.photoURL,
-          // );
         } else {
-          console.log('reutrning user');
           return 'Home';
         }
-        // console.log(p);
-        // return p;
-        //navigate to homepage here
       })
       .catch(error => {
         //Account linking will be needed with email/phone_number login
         // if (errorCode === 'auth/account-exists-with-different-credential') {
         //   alert('Email already associated with another account.');
         //   // Handle account linking here, if using.
-        console.log(`Facebook login fail with error: ${error.message}`);
+        return Promise.reject(new Error(error))
       });
-    return result;
   };
 
   //test logout
@@ -103,30 +80,22 @@ class FacebookService {
   };
 
   deleteUser = () => {
-    api
-      .deleteUser()
+    api.deleteUser()
       .then(() => {
         AccessToken.refreshCurrentAccessTokenAsync();
       })
       .then(() => {
-        return AccessToken.getCurrentAccessToken();
-      })
-      .then(data => {
+        const accessToken = AccessToken.getCurrentAccessToken();
         const credential = firebase.auth.FacebookAuthProvider.credential(
-          data.accessToken,
+          accessToken,
         );
         firebase.auth().currentUser.reauthenticateWithCredential(credential);
         firebase.auth().currentUser.delete();
+        AsyncStorage.multiRemove([NAME, USERNAME, ID, UID, EMAIL, PHOTO])
       })
       .catch(error => {
         console.log(error);
       });
-
-    AsyncStorage.multiRemove([NAME, USERNAME, ID, UID, EMAIL, PHOTO])
-      .then(ret => {
-        console.log(ret);
-      })
-      .catch(err => console.log(err));
   };
   //getUser if needed
   // getUser = (token) => {
