df719b6be3eff32db974c2b1d554eb6129010b2d
Isha G <igonugun@gmail.com>
2020-09-13 10:46:05 -0700

    Models combined

--- backend/friendsModel.js
+++ backend/friendsModel.js
@@ -1,41 +1,41 @@
-const Accounts = require("./accountsModel");
+// const Accounts = require("./accountsModel");
 
 
-const Friends = sequelize.define('friends', {
+// const Friends = sequelize.define('friends', {
 
-    main_user: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-        primaryKey: true
-    },
+//     main_user: {
+//         type: DataTypes.STRING(20),
+//         allowNull: false,
+//         primaryKey: true
+//     },
 
-    // friend_id: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull: false,
-    //     unique: true,
-    //     primaryKey: true,
-    //     references: {
-    //         model: "Accounts",
-    //         key: "id"
-    //       }
-    // },
+//     // friend_id: {
+//     //     type: DataTypes.INTEGER,
+//     //     allowNull: false,
+//     //     unique: true,
+//     //     primaryKey: true,
+//     //     references: {
+//     //         model: "Accounts",
+//     //         key: "id"
+//     //       }
+//     // },
 
-    status: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-        unique: false,
+//     status: {
+//         type: DataTypes.STRING(20),
+//         allowNull: false,
+//         unique: false,
 
-    },
+//     },
 
-});
+// });
 
-Friends.belongsTo(Accounts, {foreignKey: 'friend_user', targetKey: 'username'})
-
-
+// Friends.belongsTo(Accounts, {foreignKey: 'friend_user', targetKey: 'username'})
 
 
-// Accounts.sync({ force: true }).then(()=> {
-//     console.log("All models were synchronized successfully.");
-// })
 
-module.exports = Friends;
+
+// // Accounts.sync({ force: true }).then(()=> {
+// //     console.log("All models were synchronized successfully.");
+// // })
+
+// module.exports = Friends;
