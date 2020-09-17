dce8f2341802413d4b8e8f0fb011a23ac57da349
Hanna <hannaco@ucla.edu>
2020-09-16 20:48:14 -0700

    Profile
    designed profile page w/o functionality

--- App.js
+++ App.js
@@ -6,51 +6,63 @@
 // // //  * @flow strict-local
 // // //  */
 
+// import React from 'react';
+// import Login from './login.js';
+// import Home from './home.js';
+// import Username from './username.js';
+// import UserProfileView from './profile.js';
+// import Group from './group.js';
+// import {createStackNavigator} from 'react-navigation-stack'; // 1.0.0-beta.27
+// import {createAppContainer} from 'react-navigation';
+// import AsyncStorage from '@react-native-community/async-storage';
+// import {UID} from 'react-native-dotenv';
+
+// AsyncStorage.getItem(UID).then(value => {
+//   if (value === null) start = 'Login';
+//   else start = 'Home';
+// });
+// var start = '';
+
+// const RootStack = createStackNavigator(
+//   {
+//     Home: {
+//       screen: Home,
+//     },
+//     Login: {
+//       screen: Login,
+//     },
+//     Username: {
+//       screen: Username,
+//     },
+//     Profile: {
+//       screen: UserProfileView,
+//     },
+//     Group: {
+//       screen: Group,
+//     },
+//   },
+//   {
+//     initialRouteName: 'Login',
+//     headerMode: 'none',
+//   },
+// );
+
+// const AppContainer = createAppContainer(RootStack);
+
+// export default class App extends React.Component {
+//   render() {
+//     return <AppContainer />;
+//   }
+// }
+
 import React from 'react';
 import Login from './login.js';
 import Home from './home.js';
 import Username from './username.js';
 import UserProfileView from './profile.js';
-import Group from './group.js';
-import {createStackNavigator} from 'react-navigation-stack'; // 1.0.0-beta.27
-import {createAppContainer} from 'react-navigation';
-import AsyncStorage from '@react-native-community/async-storage';
-import {UID} from 'react-native-dotenv';
-
-AsyncStorage.getItem(UID).then(value => {
-  if (value === null) start = 'Login';
-  else start = 'Home';
-});
-var start = '';
-
-const RootStack = createStackNavigator(
-  {
-    Home: {
-      screen: Home,
-    },
-    Login: {
-      screen: Login,
-    },
-    Username: {
-      screen: Username,
-    },
-    Profile: {
-      screen: UserProfileView,
-    },
-    Group: {
-      screen: Group,
-    },
-  },
-  {
-    initialRouteName: 'Login',
-    headerMode: 'none',
-  },
-);
-
-const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return <UserProfileView />;
   }
 }
--- profile.js
+++ profile.js
@@ -6,7 +6,10 @@ import {
   Image,
   ScrollView,
   TouchableHighlight,
+  Modal,
 } from 'react-native';
+import {SearchBar} from 'react-native-elements';
+import {BlurView} from '@react-native-community/blur';
 import AsyncStorage from '@react-native-community/async-storage';
 import {NAME, USERNAME, PHOTO} from 'react-native-dotenv';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -15,15 +18,202 @@ import Swiper from 'react-native-swiper';
 const hex = '#F25763';
 const font = 'CircularStd-Medium';
 
-class Friends extends React.Component {
+const people = [
+  {
+    name: 'Hanna Co',
+    username: '@hannaco',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+  {
+    name: 'Hubert Chen',
+    username: '@hubesc',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+  {
+    name: 'Isha Gonu',
+    username: '@ishagonu',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+  {
+    name: 'Ruth Lee',
+    username: '@ruthlee',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+  {
+    name: 'Hanna Co',
+    username: '@hannaco',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+  {
+    name: 'Michelle Chan',
+    username: '@mishigan',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+  {
+    name: 'Janice Tsai',
+    username: '@jopanice',
+    image:
+      'https://i.pinimg.com/236x/ec/d3/8c/ecd38c516c19d5cf6624ce3eeae1c4b2.jpg',
+  },
+];
+
+class Card extends React.Component {
+  state = {
+    friends: this.props.friends,
+    pressed: false,
+  };
   render() {
-    return <Text>Friends</Text>;
+    return (
+      <View style={{flexDirection: 'row', flex: 1}}>
+        <Image
+          source={{
+            uri: this.props.image,
+          }}
+          style={{borderRadius: 63, height: 60, width: 60, margin: '3%'}}
+        />
+        <View
+          style={{
+            alignSelf: 'center',
+            marginLeft: '1%',
+            flex: 1,
+          }}>
+          <Text style={{fontFamily: font, fontWeight: 'bold', fontSize: 15}}>
+            {this.props.name}
+          </Text>
+          <Text style={{fontFamily: font}}>{this.props.username}</Text>
+        </View>
+        {this.state.friends && (
+          <View style={{flexDirection: 'row', flex: 1}}>
+            <Text
+              style={{
+                fontFamily: font,
+                color: hex,
+                fontSize: 15,
+                alignSelf: 'center',
+                marginLeft: '25%',
+              }}>
+              Friends
+            </Text>
+            <Icon
+              style={{
+                fontFamily: font,
+                color: hex,
+                fontSize: 35,
+                alignSelf: 'center',
+                marginLeft: '5%',
+              }}
+              name="check-circle"
+            />
+          </View>
+        )}
+        {!this.state.friends && (
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <TouchableHighlight
+              underlayColor="black"
+              onHideUnderlay={() => this.setState({pressed: false})}
+              onShowUnderlay={() => this.setState({pressed: true})}
+              onPress={() => this.setState({friends: true})}
+              style={{
+                borderColor: 'black',
+                borderRadius: 30,
+                borderWidth: 2,
+                height: '30%',
+                width: '50%',
+                marginLeft: '25%',
+                alignSelf: 'center',
+              }}>
+              <Text
+                style={{
+                  fontFamily: font,
+                  fontSize: 15,
+                  textAlign: 'center',
+                  color: this.state.pressed ? 'white' : 'black',
+                }}>
+                Accept
+              </Text>
+            </TouchableHighlight>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+class Friends extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({search});
+  };
+
+  render() {
+    const search = this.state.search;
+    const friends = [];
+    for (var i = 0; i < people.length; i++) {
+      friends.push(
+        <Card
+          key={i}
+          name={people[i].name}
+          username={people[i].username}
+          image={people[i].image}
+          friends={true}
+        />,
+      );
+    }
+    return (
+      <View>
+        <View>
+          <SearchBar
+            containerStyle={{
+              backgroundColor: 'white',
+              height: 45,
+              alignSelf: 'center',
+            }}
+            inputContainerStyle={{
+              height: 7,
+              width: '90%',
+              alignSelf: 'center',
+            }}
+            placeholder="Search by username"
+            onChangeText={this.updateSearch}
+            value={search}
+            lightTheme={true}
+            round={true}
+          />
+        </View>
+        <ScrollView style={{flexDirection: 'column'}}>{friends}</ScrollView>
+      </View>
+    );
   }
 }
 
 class Requests extends React.Component {
   render() {
-    return <Text>Requests</Text>;
+    const friends = [];
+    for (var i = 0; i < people.length; i++) {
+      friends.push(
+        <Card
+          key={i}
+          name={people[i].name}
+          username={people[i].username}
+          image={people[i].image}
+          friends={false}
+        />,
+      );
+    }
+    return (
+      <View>
+        <ScrollView style={{flexDirection: 'column'}}>{friends}</ScrollView>
+      </View>
+    );
   }
 }
 
@@ -33,6 +223,10 @@ export default class UserProfileView extends Component {
     username: '',
     photo: '',
     friends: true,
+    visible: false,
+    changeName: false,
+    changeUser: false,
+    public: false,
   };
 
   componentDidMount() {
@@ -51,7 +245,11 @@ export default class UserProfileView extends Component {
               style={styles.topIcons}
               onPress={() => this.props.navigation.navigate('Home')}
             />
-            <Icon name="cog" style={styles.topIcons} />
+            <Icon
+              name="cog"
+              style={styles.topIcons}
+              onPress={() => this.setState({visible: true})}
+            />
           </View>
           <Text style={styles.myProfile}>My Profile</Text>
           <View style={styles.userInfo}>
@@ -94,7 +292,7 @@ export default class UserProfileView extends Component {
             </TouchableHighlight>
           </View>
         </View>
-        <View style={{backgroundColor: hex, height: '100%', marginTop: '5%'}}>
+        <View style={{height: '100%', marginTop: '5%'}}>
           <Swiper
             loop={false}
             onMomentumScrollEnd={() =>
@@ -104,6 +302,160 @@ export default class UserProfileView extends Component {
             <Requests />
           </Swiper>
         </View>
+        {this.state.visible && (
+          <BlurView
+            blurType="light"
+            blurAmount={20}
+            reducedTransparencyFallbackColor="white"
+            style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}
+          />
+        )}
+        {this.state.visible && (
+          <Modal
+            animationType={'fade'}
+            visible={this.state.visible}
+            transparent={true}>
+            <View style={styles.modal}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  margin: '5%',
+                }}>
+                <Text
+                  style={{
+                    fontFamily: font,
+                    fontSize: 18,
+                    color: hex,
+                    alignSelf: 'center',
+                  }}>
+                  Settings
+                </Text>
+                <Icon
+                  name="times-circle"
+                  style={{color: hex, fontFamily: font, fontSize: 30}}
+                  onPress={() => this.setState({visible: false})}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  margin: '5%',
+                }}>
+                <View>
+                  <Text style={{fontFamily: font, fontSize: 18}}>Name:</Text>
+                  <Text style={{fontFamily: font, color: hex, fontSize: 20}}>
+                    Hubert Chen
+                  </Text>
+                </View>
+                <TouchableHighlight
+                  underlayColor={hex}
+                  onShowUnderlay={() => this.setState({changeName: true})}
+                  onHideUnderlay={() => this.setState({changeName: false})}
+                  onPress={() => console.log('change name')}
+                  style={styles.changeButtons}>
+                  <Text
+                    style={
+                      this.state.changeName
+                        ? styles.changeTextSelected
+                        : styles.changeText
+                    }>
+                    Change
+                  </Text>
+                </TouchableHighlight>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  margin: '5%',
+                }}>
+                <View>
+                  <Text style={{fontFamily: font, fontSize: 18}}>
+                    Username:
+                  </Text>
+                  <Text style={{fontFamily: font, color: hex, fontSize: 20}}>
+                    @hubesc
+                  </Text>
+                </View>
+                <TouchableHighlight
+                  style={styles.changeButtons}
+                  underlayColor={hex}
+                  onShowUnderlay={() => this.setState({changeUser: true})}
+                  onHideUnderlay={() => this.setState({changeUser: false})}
+                  onPress={() => console.log('change user')}>
+                  <Text
+                    style={
+                      this.state.changeUser
+                        ? styles.changeTextSelected
+                        : styles.changeText
+                    }>
+                    Change
+                  </Text>
+                </TouchableHighlight>
+              </View>
+              <View
+                style={{
+                  margin: '5%',
+                }}>
+                <Text style={{fontFamily: font, fontSize: 18}}>
+                  Friends List Display:
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    margin: '5%',
+                  }}>
+                  <TouchableHighlight
+                    underlayColor={hex}
+                    onShowUnderlay={() => this.setState({public: true})}
+                    style={{
+                      alignSelf: 'center',
+                      borderWidth: 2,
+                      borderColor: hex,
+                      borderRadius: 50,
+                      width: '35%',
+                      marginRight: '5%',
+                      backgroundColor: this.state.public ? hex : 'white',
+                    }}
+                    onPress={() => this.setState({public: true})}>
+                    <Text
+                      style={
+                        this.state.public
+                          ? styles.changeTextSelected
+                          : styles.changeText
+                      }>
+                      Public
+                    </Text>
+                  </TouchableHighlight>
+                  <TouchableHighlight
+                    underlayColor={hex}
+                    onShowUnderlay={() => this.setState({public: false})}
+                    style={{
+                      alignSelf: 'center',
+                      borderWidth: 2,
+                      borderColor: hex,
+                      borderRadius: 50,
+                      width: '35%',
+                      backgroundColor: this.state.public ? 'white' : hex,
+                    }}
+                    onPress={() => this.setState({public: false})}>
+                    <Text
+                      style={
+                        this.state.public
+                          ? styles.changeText
+                          : styles.changeTextSelected
+                      }>
+                      Private
+                    </Text>
+                  </TouchableHighlight>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        )}
       </View>
     );
   }
@@ -161,5 +513,37 @@ const styles = StyleSheet.create({
     paddingRight: '3%',
     paddingTop: '0.5%',
     paddingBottom: '0.5%',
+  },
+  modal: {
+    height: '50%',
+    width: '75%',
+    margin: '3%',
+    backgroundColor: 'white',
+    alignSelf: 'flex-end',
+    borderRadius: 30,
+    elevation: 20,
+  },
+  changeButtons: {
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: hex,
+    borderRadius: 50,
+    width: '35%',
+  },
+  changeText: {
+    fontFamily: font,
+    color: hex,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingTop: '2.5%',
+    paddingBottom: '2.5%',
+  },
+  changeTextSelected: {
+    fontFamily: font,
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 17,
+    paddingTop: '2.5%',
+    paddingBottom: '2.5%',
   },
 });
