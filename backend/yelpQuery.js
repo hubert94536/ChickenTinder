80a87f3f4ead17f52a3f4e43290d2c76d8ee392e
Hubert Chen <hubert94536@yahoo.com>
2020-09-19 16:51:43 -0700

    Group refactoring and backend updates

---
+++ backend/yelpQuery.js
@@ -0,0 +1,41 @@
+// setting up Yelp API base caller
+const yelpApi = axios.create({
+  baseURL: 'https://api.yelp.com/v3',
+  headers: {
+    Authorization: `Bearer ${process.env.YELP_KEY}`
+  }
+})
+
+// getting list of resturants
+const getRestaurants = (req) => {
+  return (
+    yelpApi
+      .get('/businesses/search', req)
+      // returns business info from Yelp
+      .then(res => {
+        return {
+          total: res.data.total,
+          businessList: res.data.businesses.map(function (business) {
+            return {
+              name: business.name,
+              distance: business.distance,
+              categories: business.categories,
+              reviewCount: business.review_count,
+              rating: business.rating,
+              price: business.price,
+              phone: business.display_phone,
+              location: business.location,
+              isClosed: business.is_closed
+            }
+          })
+        }
+      })
+      .catch(error => {
+        return Promise.reject(new Error(error.response.status))
+      })
+  )
+}
+
+module.exports = {
+  getRestaurants
+}
