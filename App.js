37c6c65051e801ffa7f626f0e394c6ffb2364264
Hubert Chen <hubert94536@yahoo.com>
2020-09-19 23:44:58 -0700

    fix group warning, delete,logout

--- App.js
+++ App.js
@@ -6,20 +6,20 @@
 // // //  * @flow strict-local
 // // //  */
 
-import React from 'react';
-import Login from './login.js';
-import Home from './home.js';
-import Username from './username.js';
-import UserProfileView from './profile.js';
-import Group from './group.js';
-import {createStackNavigator} from 'react-navigation-stack'; // 1.0.0-beta.27
-import {createAppContainer} from 'react-navigation';
-import AsyncStorage from '@react-native-community/async-storage';
-import {UID} from 'react-native-dotenv';
+import React from 'react'
+import Login from './login.js'
+import Home from './home.js'
+import Username from './username.js'
+import UserProfileView from './profile.js'
+import Group from './group.js'
+import {createStackNavigator} from 'react-navigation-stack' // 1.0.0-beta.27
+import {createAppContainer} from 'react-navigation'
+import AsyncStorage from '@react-native-community/async-storage'
+import {UID} from 'react-native-dotenv'
 
 AsyncStorage.getItem(UID).then(value => {
-  if (value === null) start = 'Login';
-  else start = 'Home';
+  if (value === null) start = 'Login'
+  else start = 'Home'
 });
 var start = '';
 
@@ -42,7 +42,7 @@ const RootStack = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: start,
     headerMode: 'none',
   },
 );
@@ -54,13 +54,3 @@ export default class App extends React.Component {
     return <AppContainer />;
   }
 }
-
-// import React from 'react';
-// import {Button} from 'react-native';
-// import Group from './group.js';
-
-// export default class App extends React.Component {
-//   render() {
-//     return <Group />;
-//   }
-// }
