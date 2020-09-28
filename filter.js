cefb068064cd7baf9d1e52a67c00d08f19297a0f
Hanna <hannaco@ucla.edu>
2020-09-28 12:48:30 -0700

    filters
    fixed filters page and modified format of filters

--- filter.js
+++ filter.js
@@ -20,6 +20,7 @@ import Geolocation from 'react-native-geolocation-service';
 import TagsView from './TagsView';
 import Slider from 'react-native-slider';
 import BackgroundButton from './BackgroundButton';
+import TimePicker from 'react-native-simple-time-picker';
 
 const hex = '#F25763';
 const font = 'CircularStd-Bold';
@@ -36,8 +37,6 @@ const tagsCuisine = [
   'Indian',
   'Vietnamese',
 ];
-
-const tagsDining = ['Dine-in', 'Delivery', 'Catering', 'Pickup'];
 
 const tagsPrice = ['$', '$$', '$$$', '$$$$', 'Any'];
 
@@ -81,11 +80,14 @@ export default class FilterSelector extends React.Component {
     this.state = {
       isHost: true,
       distance: 5,
+      hour: 23,
+      minute: 59,
+      time: 235900, //unix time
       lat: 0,
       long: 0,
-      selectedCuisine: [],
-      selectedDining: [],
-      selectedPrice: [],
+      selectedCuisine: [], //array
+      selectedPrice: [], //string "1,2,3"
+      price: null, //string of prices
       selectedDiet: [],
     };
   }
@@ -107,6 +109,50 @@ export default class FilterSelector extends React.Component {
     }
   }
 
+  evaluateFilters() {
+    this.setState({
+      time: parseInt(
+        JSON.stringify(this.state.hour) +
+          JSON.stringify(this.state.minute) +
+          '00',
+      ),
+    });
+    var prices = '';
+    for (var i = 0; i < this.state.selectedPrice.length; i++) {
+      switch (this.state.selectedPrice[i]) {
+        case '$':
+          prices += '1';
+          break;
+        case '$$':
+          if (prices === '') {
+            prices += '2';
+          } else {
+            prices += ',2';
+          }
+          break;
+        case '$$$':
+          if (prices === '') {
+            prices += '3';
+          } else {
+            prices += ',3';
+          }
+          break;
+        case '$$$$':
+          if (prices === '') {
+            prices += '4';
+          } else {
+            prices += ',4';
+          }
+          break;
+        case 'Any':
+          prices = '1,2,3,4';
+          break;
+      }
+    }
+    this.setState({price: prices});
+    console.log(this.state.price);
+  }
+
   render() {
     return (
       <View style={styles.mainContainer}>
@@ -118,89 +164,97 @@ export default class FilterSelector extends React.Component {
             <Text style={styles.titleSub}>(only visible to host)</Text>
           )}
         </View>
-        {this.state.isHost && (
-          <View style={{flex: 0.5, margin: '2%'}}>
-            <Text style={styles.header}>Members</Text>
-            <TouchableHighlight
-              underlayColor={'white'}
-              style={styles.touchableFriends}>
-              <Text style={styles.touchableFriendsText}>
-                Select from Friends
-              </Text>
-            </TouchableHighlight>
-          </View>
-        )}
-        <View style={{flex: 1, margin: '2%'}}>
-          <Text style={this.state.isHost ? styles.header : styles.headerFalse}>
-            Cuisines
-          </Text>
-          <TagsView
-            all={tagsCuisine}
-            selected={this.state.selectedCuisine}
-            isExclusive={false}
-          />
-        </View>
-        {this.state.isHost && (
-          <View style={{flex: 0.5, margin: '2%'}}>
-            <View style={{flexDirection: 'row'}}>
-              <Text style={styles.header}>Distance</Text>
-              <Text
-                style={{
-                  color: 'white',
-                  fontFamily: font,
-                  alignSelf: 'center',
-                  marginLeft: '1%',
-                }}>
-                ({this.state.distance} miles)
-              </Text>
+        <ScrollView>
+          {this.state.isHost && (
+            <View style={{margin: '2%'}}>
+              <Text style={styles.header}>Members</Text>
+              <TouchableHighlight
+                underlayColor={'white'}
+                style={styles.touchableFriends}>
+                <Text style={styles.touchableFriendsText}>
+                  Select from Friends
+                </Text>
+              </TouchableHighlight>
             </View>
-            <Slider
-              style={{
-                width: '85%',
-                height: 30,
-                alignSelf: 'center',
-              }}
-              minimumValue={5}
-              maximumValue={25}
-              value={5}
-              step={0.5}
-              minimumTrackTintColor="white"
-              thumbTintColor="white"
-              onValueChange={value => this.setState({distance: value})}
-            />
-          </View>
-        )}
-        {this.state.isHost && (
-          <View style={{flex: 0.5, margin: '2%'}}>
-            <Text style={styles.header}>Dining Options</Text>
+          )}
+          <View style={{margin: '2%'}}>
+            <Text style={styles.header}>Cuisines</Text>
             <TagsView
-              all={tagsDining}
-              selected={this.state.selectedDining}
+              all={tagsCuisine}
+              selected={this.state.selectedCuisine}
               isExclusive={false}
             />
           </View>
-        )}
-        {this.state.isHost && (
-          <View style={{flex: 0.5, margin: '2%'}}>
-            <Text style={styles.header}>Price</Text>
+          {this.state.isHost && (
+            <View style={{margin: '2%'}}>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={styles.header}>Distance</Text>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontFamily: font,
+                    alignSelf: 'center',
+                    marginLeft: '1%',
+                  }}>
+                  ({this.state.distance} miles)
+                </Text>
+              </View>
+              <Slider
+                style={{
+                  width: '85%',
+                  height: 30,
+                  alignSelf: 'center',
+                }}
+                minimumValue={5}
+                maximumValue={25}
+                value={5}
+                step={0.5}
+                minimumTrackTintColor="white"
+                thumbTintColor="white"
+                onValueChange={value => this.setState({distance: value})}
+              />
+            </View>
+          )}
+          {this.state.isHost && (
+            <View style={{margin: '2%'}}>
+              <Text style={styles.header}>
+                Open at: {this.state.hour}:{this.state.minute}
+              </Text>
+              <TimePicker
+                selectedHours={this.state.hour}
+                selectedMinutes={this.state.minute}
+                onChange={(hours, minutes) =>
+                  this.setState({
+                    hour: hours,
+                    minute: minutes,
+                  })
+                }
+              />
+            </View>
+          )}
+          {this.state.isHost && (
+            <View style={{margin: '2%'}}>
+              <Text style={styles.header}>Price</Text>
+              <TagsView
+                all={tagsPrice}
+                selected={this.state.selectedPrice}
+                isExclusive={false}
+              />
+            </View>
+          )}
+          <View style={{margin: '2%'}}>
+            <Text style={styles.header}>Dietary Restrictions</Text>
             <TagsView
-              all={tagsPrice}
-              selected={this.state.selectedPrice}
+              all={tagsDiet}
+              selected={this.state.selectedDiet}
               isExclusive={false}
             />
           </View>
-        )}
-        <View style={{flex: 1, margin: '2%'}}>
-          <Text style={this.state.isHost ? styles.header : styles.headerFalse}>
-            Dietary Restrictions
-          </Text>
-          <TagsView
-            all={tagsDiet}
-            selected={this.state.selectedDiet}
-            isExclusive={false}
-          />
-        </View>
-        <TouchableHighlight underlayColor={'white'} style={styles.touchable}>
+        </ScrollView>
+        <TouchableHighlight
+          underlayColor={'white'}
+          style={styles.touchable}
+          onPress={() => this.evaluateFilters()}>
           <Text style={styles.nextTitle}>
             {this.state.isHost ? "Let's Go" : 'Submit Filters'}
           </Text>
@@ -219,8 +273,7 @@ const styles = StyleSheet.create({
   },
   titleStyle: {
     flexDirection: 'row',
-    flex: 0.5,
-    margin: '2%',
+    margin: '5%',
     alignItems: 'center',
   },
   titleText: {
@@ -240,7 +293,7 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     alignSelf: 'center',
     width: '60%',
-    margin: '1%',
+    marginTop: '5%',
   },
   touchableFriendsText: {
     color: 'white',
@@ -253,13 +306,8 @@ const styles = StyleSheet.create({
   header: {
     textAlign: 'left',
     color: 'white',
-    fontSize: 18,
-    fontFamily: font,
-  },
-  headerFalse: {
-    textAlign: 'left',
-    color: 'white',
     fontSize: 25,
+    margin: '1%',
     fontFamily: font,
   },
   touchable: {
@@ -269,7 +317,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 25,
     justifyContent: 'center',
-    margin: '4%',
+    margin: '5%',
   },
   nextTitle: {
     textAlign: 'center',
