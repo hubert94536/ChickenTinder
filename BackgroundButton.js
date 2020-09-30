d695165896ed516c30d037c0c307706a25ace126
Hubert Chen <30395134+hubert94536@users.noreply.github.com>
2020-09-30 00:54:53 -0700

    Filters (#50)
    * Filters
    
    fixed the dimensions, added geolocation and connected groups and filters
    
    * Invite
    
    created invite notif
    
    * filter
    
    removed card from filter screen and changed it to host and member view
    
    * filters
    
    fixed filters page and modified format of filters
    
    * filter
    
    fixed the time picker
    
    * Update App.js
    
    * Update App.js
    
    * filter and groups
    
    fixed some dimensions
    
    * Update group.js
    
    Co-authored-by: Hanna <hannaco@ucla.edu>

--- BackgroundButton.js
+++ BackgroundButton.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { TouchableOpacity, View, Text, StyleSheet, Image } from 'react-native'
+import React from 'react';
+import {TouchableOpacity, View, Text, StyleSheet, Image} from 'react-native';
 // import R from 'res/R'
 export default class BackgroundButton extends React.Component {
   render() {
-    const styles = this.makeStyles()
+    const styles = this.makeStyles();
     return (
       <TouchableOpacity style={styles.touchable} onPress={this.props.onPress}>
         <View style={styles.view}>
           <Text style={styles.text}>{this.props.title}</Text>
         </View>
       </TouchableOpacity>
-    )
+    );
   }
-// makeImageIfAny(styles) {
-//     if (this.props.showImage) {
-//       return <Image style={styles.image} source={R.images.check} />
-//     }
-//   }
-makeStyles() {
+  // makeImageIfAny(styles) {
+  //     if (this.props.showImage) {
+  //       return <Image style={styles.image} source={R.images.check} />
+  //     }
+  //   }
+  makeStyles() {
     return StyleSheet.create({
       view: {
         flexDirection: 'row',
         // borderRadius: 23,
         borderRadius: 14,
         borderColor: this.props.borderColor,
-        borderWidth: 1,
+        borderWidth: 2,
         backgroundColor: this.props.backgroundColor,
         // height: 46,
         height: 28,
@@ -33,28 +33,30 @@ makeStyles() {
         // paddingLeft: 16,
         // paddingRight: 16
 
-        paddingHorizontal: 8
+        paddingHorizontal: 8,
       },
       touchable: {
         // marginLeft: 4,
         marginRight: 8,
-        marginBottom: 6
+        marginBottom: 6,
 
         // marginLeft: 3,
         // marginRight: 3,
         // marginBottom: 6
       },
       image: {
-        marginRight: 8
+        marginRight: 8,
       },
       text: {
         // fontSize: 5,
+        paddingLeft: '2%',
+        paddingRight: '2%',
         textAlign: 'center',
         color: this.props.textColor,
         // fontSize: 16
-        fontSize: 13,
-        fontFamily: 'CircularStd-Book'
-      }
-    })
+        fontSize: 15,
+        fontFamily: 'CircularStd-Bold',
+      },
+    });
   }
 }
