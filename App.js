28ca504dfb3d7abd2f3db0ed83a196e2601c325c
Hubert Chen <30395134+hubert94536@users.noreply.github.com>
2020-10-09 23:59:30 -0700

    Filters (#60)
    * time
    
    fixed unix time to be in seconds
    
    * tags
    
    tags selecting not updating fixed
    
    * fixed card
    
    Co-authored-by: Hanna <hannaco@ucla.edu>

--- App.js
+++ App.js
@@ -1,55 +1,65 @@
+// import React from 'react';
+// import Login from './login.js';
+// import Home from './home.js';
+// import Username from './username.js';
+// import UserProfileView from './profile.js';
+// import Group from './group.js';
+// import RestaurantCard from './round.js';
+// import {UID} from 'react-native-dotenv';
+// import firebase from 'firebase';
+// import {createStackNavigator} from 'react-navigation-stack'; // 1.0.0-beta.27
+// import {createAppContainer} from 'react-navigation';
+
+// var user = firebase.auth().currentUser;
+// var start = '';
+
+// if (user === null) {
+//   start = 'Login';
+// } else {
+//   start = 'Home';
+// }
+
+// const RootStack = createStackNavigator(
+//   {
+//     Home: {
+//       screen: Home,
+//     },
+//     Login: {
+//       screen: Login,
+//     },
+//     Username: {
+//       screen: Username,
+//     },
+//     Profile: {
+//       screen: UserProfileView,
+//     },
+//     Group: {
+//       screen: Group,
+//     },
+//     Round: {
+//       screen: RestaurantCard,
+//     },
+//   },
+//   {
+//     initialRouteName: start,
+//     headerMode: 'none',
+//   },
+// );
+
+// const AppContainer = createAppContainer(RootStack);
+
+// export default class App extends React.Component {
+//   render() {
+//     return <AppContainer />;
+//   }
+// }
+
 import React from 'react';
-import Login from './login.js';
-import Home from './home.js';
-import Username from './username.js';
-import UserProfileView from './profile.js';
-import Group from './group.js';
-import RestaurantCard from './round.js';
-import {UID} from 'react-native-dotenv';
-import firebase from 'firebase';
-import {createStackNavigator} from 'react-navigation-stack'; // 1.0.0-beta.27
-import {createAppContainer} from 'react-navigation';
-
-var user = firebase.auth().currentUser;
-var start = '';
-
-if (user === null) {
-  start = 'Login';
-} else {
-  start = 'Home';
-}
-
-const RootStack = createStackNavigator(
-  {
-    Home: {
-      screen: Home,
-    },
-    Login: {
-      screen: Login,
-    },
-    Username: {
-      screen: Username,
-    },
-    Profile: {
-      screen: UserProfileView,
-    },
-    Group: {
-      screen: Group,
-    },
-    Round: {
-      screen: RestaurantCard,
-    },
-  },
-  {
-    initialRouteName: 'Login',
-    headerMode: 'none',
-  },
-);
-
-const AppContainer = createAppContainer(RootStack);
+import FilterSelector from './filter.js';
+import Round from './round.js';
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return <Round />;
   }
 }
