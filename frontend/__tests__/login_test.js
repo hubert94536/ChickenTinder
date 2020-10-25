50797663b00f96c7203ca6f7c97b79031051c161
Isha G <igonugun@gmail.com>
2020-10-25 11:51:36 -0700

    Login test

---
+++ frontend/__tests__/login_test.js
@@ -0,0 +1,56 @@
+import 'react-native';
+import React from 'react';
+import Login from '../screens/login';
+import renderer from 'react-test-renderer';
+// import accountsApi from '../apis/accountsApi.js'
+import MockAsyncStorage from 'mock-async-storage';
+import { Item } from 'react-native-paper/lib/typescript/src/components/List/List';
+// import facebookService from '../apis/facebookService.js'
+ 
+
+
+const facebookService = require('../apis/facebookService.js')
+jest.mock('../apis/facebookService.js')
+
+// it('returns username', async () => {
+//   const username = 'Username'
+//   retVal = facebookService.loginWithFacebook.mockResolvedValue(username);
+
+//   // const retVal = await facebookService.loginWithFacebook();
+//   expect(retVal).toEqual('Username');
+// });
+
+test('renders correctly', () => {
+    facebookService.loginWithFacebook.mockResolvedValue('Username')
+    const tree = renderer.create(<Login />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+// import 'react-native';
+// import MockAsyncStorage from 'mock-async-storage'
+// import React from 'react';
+// import Login from '../screens/login'
+ 
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
+ 
+// const mock = () => {
+//   const mockImpl = new MockAsyncStorage()
+//   jest.mock('AsyncStorage', () => mockImpl)
+// }
+ 
+// mock();
+ 
+// import { AsyncStorage as storage } from 'react-native'
+ 
+// it('renders correctly', () => {
+//   const tree = renderer.create(
+//     <Index />
+//   );
+// });
+ 
+// it('Mock Async Storage working', async () => {
+//   await storage.setItem('myKey', 'myValue')
+//   const value = await storage.getItem('myKey')
+//   expect(value).toBe('myValue')
+// })
