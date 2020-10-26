de142704ea45659672acf32218bc3bb684a02333
Isha G <igonugun@gmail.com>
2020-10-25 19:35:33 -0700

    Login test with username

--- frontend/__mocks__/facebookService.js
+++
@@ -1,3 +0,0 @@
-const loginWithFacebook = async () => {
-    return Promise.resolve('Username')
-}
--- frontend/__tests__/login_test.js
+++ frontend/__tests__/login_test.js
@@ -2,57 +2,16 @@ import 'react-native';
 import React from 'react';
 import Login from '../screens/login';
 import renderer from 'react-test-renderer';
-// import accountsApi from '../apis/accountsApi.js'
-// import MockAsyncStorage from 'mock-async-storage';
-import { Item } from 'react-native-paper/lib/typescript/src/components/List/List';
-// import facebookService from '../apis/facebookService.js'
+
  
 
 
 const facebookService = require('../apis/facebookService.js')
 jest.mock('../apis/facebookService.js')
 
-test('should return username', () => {
-  
-  facebookService.loginWithFacebook().then(retVal => {
-    expect(retVal).toEqual('Username');
+
+test('renders correctly', () => {
+    const tree = renderer.create(<Login />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
-  // const retVal = await facebookService.loginWithFacebook();
-  
-});
-
-// test('renders correctly', () => {
-//     // facebookService.loginWithFacebook.mockResolvedValue('Username')
-//     const tree = renderer.create(<Login />).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-
-// import 'react-native';
-// import MockAsyncStorage from 'mock-async-storage'
-// import React from 'react';
-// import Login from '../screens/login'
- 
-// // Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
- 
-// const mock = () => {
-//   const mockImpl = new MockAsyncStorage()
-//   jest.mock('AsyncStorage', () => mockImpl)
-// }
- 
-// mock();
- 
-// import { AsyncStorage as storage } from 'react-native'
- 
-// it('renders correctly', () => {
-//   const tree = renderer.create(
-//     <Index />
-//   );
-// });
- 
-// it('Mock Async Storage working', async () => {
-//   await storage.setItem('myKey', 'myValue')
-//   const value = await storage.getItem('myKey')
-//   expect(value).toBe('myValue')
-// })
