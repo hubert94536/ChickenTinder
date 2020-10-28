0d8118779709a369cf6c396d43c2f98140204958
Megan Schmi <meganyschmid@gmail.com>
2020-10-28 01:01:31 -0700

    fixed code for returns and removed fail functions

--- frontend/accountsApiMock.js
+++ frontend/accountsApiMock.js
@@ -1,285 +1,142 @@
-import { ID } from 'react-native-dotenv'
-import AsyncStorage from '@react-native-community/async-storage'
-import axios from 'axios'
+// mock functions for accountsApi
 
 // creates user and returns id
-const createFBUser = async (name, id, username, email, photo) => {
-    return 200
+const createFBUser = (name, id, username, email, photo) => {
+    return Promise.resolve(200)
 }
 
 // gets list of users
-const getAllUsers = async () => {
-    return {
+const getAllUsers = () => {
+    return Promise.resolve({
         status: 200,
         userList: [  // returns individual user info
             {
-                name: "John",
+                name: "John Wick",
                 username: "j0hn",
-                photo: "photo1",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "1",
             },
             {
-                name: "Bobby",
+                name: "Bobby Brown",
                 username: "b0bby",
-                photo: "photo2",
+                photo: "phohttps://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038to2",
                 id: "2",
             },
             {
-                name: "Caitlin",
+                name: "Caitlin Lee",
                 username: "cait1in",
-                photo: "photo3",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "3",
             },
             {
-                name: "Sasha",
+                name: "Sasha Suresh",
                 username: "sa5ha",
-                photo: "photo4",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "4",
             },
             {
-                name: "Ashley",
+                name: "Ashley Nguyen",
                 username: "ashl3y",
-                photo: "photos5",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "5",
             }
         ]
-      }
+      })
 } 
 
 // gets first 100 account usernames/names starting with text input
 // mock function calls 5
-const searchUsers = async (text) => {
-    return {
+const searchUsers = (text) => {
+    return Promise.resolve({
         status: 200,
         count: 5,
         userList: [  // returns individual user info
             {
-                name: "John",
+                name: "John Wick",
                 username: "j0hn",
-                photo: "photo1",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "1",
             },
             {
-                name: "Bobby",
+                name: "Bobby Brown",
                 username: "b0bby",
-                photo: "photo2",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "2",
             },
             {
-                name: "Caitlin",
+                name: "Caitlin Lee",
                 username: "cait1in",
-                photo: "photo3",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "3",
             },
             {
-                name: "Sasha",
+                name: "Sasha Suresh",
                 username: "sa5ha",
-                photo: "photo4",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "4",
             },
             {
-                name: "Ashley",
+                name: "Ashley Nguyen",
                 username: "ashl3y",
-                photo: "photos5",
+                photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/cp0/c15.0.50.50a/p50x50/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=ZEui8MAu8YMAX8gwK_s&_nc_ht=scontent-lax3-1.xx&tp=27&oh=37a708095535bfb06a83a65981aee779&oe=5FC08038",
                 id: "5",
             }
         ]
-      }
+      })
 }
 
 // deletes user and returns status
-const deleteUser = async () => {
-    return 200
+const deleteUser = () => {
+    return Promise.resolve(200)
 }
 
 // gets user by id and returns user info
-const getUser = async (id) => {
-    return {
+const getUser = (id) => {
+    return Promise.resolve({
         status: 200,
         username: "j0hn",
         email: "j0hn@gmail.com",
         phone_number: 9491234567,
-        name: "John",
+        name: "John Wick",
         photo: "photoJohn",
         id: 1,
-      }
+      })
 }
 
 // update email and returns status
-const updateEmail = async (info) => {
-    return 200
+const updateEmail = (info) => {
+    return Promise.resolve(200)
 }
 
 // update username and returns status
-const updateUsername = async (info) => {
-    return 200
+const updateUsername = (info) => {
+    return Promise.resolve(200)
 }
 
 // update username and returns status
-const updateName = async (info) => {
-    return 200
+const updateName = (info) => {
+    return Promise.resolve(200)
 }
 
 // update user name and returns status
-const updatePhoneNumber = async (info) => {
-    return 200
+const updatePhoneNumber = (info) => {
+    return Promise.resolve(200)
 }
 
 // updates user and returns status
-const updateUser = async (req) => {
-    return 200
+const updateUser = (req) => {
+    return Promise.resolve(200)
 }
 
 // checks username and returns status
-const checkUsername = async (username) => {
-    return 200
+const checkUsername = (username) => {
+    return Promise.resolve(200)
 }
 
 // checks phone number and returns status
-const checkPhoneNumber = async (phoneNumber) => {
-    return 200
+const checkPhoneNumber = (phoneNumber) => {
+    return Promise.resolve(200)
 }
-
-// Fails
-
-// FAIL: creates user and returns id
-const createFBUserFail = async (name, id, username, email, photo) => {
-    return 500
-}
-
-// FAIL: gets list of users
-const getAllUsersFail = async () => {
-    return {
-        status: 500,
-        userList: [  // returns individual user info
-            {
-                name: "John",
-                username: "j0hn",
-                photo: "photo1",
-                id: "1",
-            },
-            {
-                name: "Bobby",
-                username: "b0bby",
-                photo: "photo2",
-                id: "2",
-            },
-            {
-                name: "Caitlin",
-                username: "cait1in",
-                photo: "photo3",
-                id: "3",
-            },
-            {
-                name: "Sasha",
-                username: "sa5ha",
-                photo: "photo4",
-                id: "4",
-            },
-            {
-                name: "Ashley",
-                username: "ashl3y",
-                photo: "photos5",
-                id: "5",
-            }
-        ]
-      }
-} 
-
-// FAIL: gets first 100 account usernames/names starting with text input
-// mock function calls 5
-const searchUsersFail = async (text) => {
-    return {
-        status: 500,
-        count: 5,
-        userList: [  // returns individual user info
-            {
-                name: "John",
-                username: "j0hn",
-                photo: "photo1",
-                id: "1",
-            },
-            {
-                name: "Bobby",
-                username: "b0bby",
-                photo: "photo2",
-                id: "2",
-            },
-            {
-                name: "Caitlin",
-                username: "cait1in",
-                photo: "photo3",
-                id: "3",
-            },
-            {
-                name: "Sasha",
-                username: "sa5ha",
-                photo: "photo4",
-                id: "4",
-            },
-            {
-                name: "Ashley",
-                username: "ashl3y",
-                photo: "photos5",
-                id: "5",
-            }
-        ]
-      }
-}
-
-// FAIL: deletes user and returns status
-const deleteUserFail = async () => {
-    return 500
-}
-
-// FAIL: gets user by id and returns user info
-const getUserFail = async (id) => {
-    return {
-        status: 500,
-        username: "j0hn",
-        email: "j0hn@gmail.com",
-        phone_number: 9491234567,
-        name: "John",
-        photo: "photoJohn",
-        id: 1,
-      }
-}
-
-// FAIL: update email and returns status
-const updateEmailFail = async (info) => {
-    return 500
-}
-
-// FAIL: update username and returns status
-const updateUsernameFail = async (info) => {
-    return 500
-}
-
-// FAIL: update user name and returns status
-const updateNameFail = async (info) => {
-    return 500
-}
-
-// FAIL: update username and returns status
-const updatePhoneNumberFail = async (info) => {
-    return 500
-}
-
-// FAIL: updates user and returns status
-const updateUserFail = async (req) => {
-    return 500
-}
-
-// FAIL: checks username and returns status
-const checkUsernameFail = async (username) => {
-    return 500
-}
-
-// FAIL: checks phone number and returns status
-const checkPhoneNumberFail = async (phoneNumber) => {
-    return 500
-}
-
 
 export default {
     createFBUser,
--- frontend/asyncStorageMock.js
+++ frontend/asyncStorageMock.js
@@ -1,20 +1,6 @@
-import React from 'react'
-import { StyleSheet, Text, TouchableHighlight, View } from 'react-native'
-import { NAME, PHOTO, USERNAME, ID } from 'react-native-dotenv'
-import AsyncStorage from '@react-native-community/async-storage'
-import accountsApi from '../apis/accountsApi.js'
-import Alert from '../modals/alert.js'
-import friendsApi from '../apis/friendsApi.js'
-import Invite from '../modals/invite.js'
-import socket from '../apis/socket.js'
+// mock functions for asyncStroage
 
-//  gets user info
-AsyncStorage.getItem(PHOTO).then((res) => ("photo1" = res))
-AsyncStorage.getItem(NAME).then((res) => ("Cat" = res))
-AsyncStorage.getItem(USERNAME).then((res) => ("c4t" = res))
+getItem=(text)=>{return Promise.resolve(text)}
+setItem=(text) => {return Promise.resolve(text)}
 
-AsyncStorage.getItem(ID).then((res) => {
-  1 = res
-})
-
-export default AsyncStorage
+export default { getItem, setItem }
