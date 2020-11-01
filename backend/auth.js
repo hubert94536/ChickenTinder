7a6f3d6bdcb6ac18a9ef076b26606144427f087c
Justin He <justinme100@gmail.com>
2020-11-01 11:39:36 -0800

    Implemented refresh tokens

--- backend/auth.js
+++ backend/auth.js
@@ -1,40 +1,38 @@
-// Tokens used only for authorizing API Requests
+// Access tokens for authorizing API Requests, Refresh tokens to generate new access tokens
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
-var privateKey = process.env.PRIVATE_KEY;
-var publicKey = process.env.PUBLIC_KEY;
+// ISSUE: .env current cannot read multiline inputs
+const privateKey = process.env.PRIVATE_KEY;
+const publicKey = process.env.PUBLIC_KEY;
+const secret = process.env.JWT_SECRET;
 
-// TODO: Most API calls will have to be verified (so must send token and possibly uid)
-// TODO: Implement Refresh Token generation and validation (for generating access tokens)
-
-
-// Generate and returns user token
-// Use only with login or refresh - no independent api endpoint
-const generateJWT = (uid) => {
+// Generate and returns access token
+// TODO: Figure out when to generate access token
+const getAccessToken = (req, res) => {
     try {
+        const uid = decodeRefreshToken(req);
         var payload = {
             uid: uid
         }
         var options = {
-            algorithm: "RS256", //CHANGE LATER
+            algorithm: "RS256", 
             expiresIn: "1d", 
             issuer: "Wechews",
             audience: "https://wechews.herokuapp.com"
         }
-        return jwt.sign(payload, privateKey, options);
+        return res.status(200).send(jwt.sign(payload, privateKey, options));
     } catch (error) {
-        throw error;
+        return res.status(500).send(error.message);
     }
 }
 
-// Middleware: Decodes token and returns user id
-// JWT MUST be attached to Access-Token for every request header 
-// Notes that requests cannot have a pre-set 'uid' in their body
-const decodeJWT = (req, res, next) => {
-    const token = req.header("Access-Token")
-    if (!token) return next();
+// Middleware: Decodes access token and returns user id
+// 'Access-Token' MUST BE ATTACHED to every request header 
+const decodeAccessToken = (req, res, next) => {
     try{
+        const token = req.header("Access-Token")
+        if (!token) return next();
         var options = {
             algorithms: ["RS256"],
             maxAge: "1d",
@@ -43,12 +41,108 @@ const decodeJWT = (req, res, next) => {
         }
         var payload = jwt.verify(token, publicKey, options);
         // consider checking db to see if uid exists
-        req.uid = payload.uid
+        req.uid = payload.uid // NOTE: Unsure if this will work as intended
         return next();
     } catch (error) {
         return next();
     }
 }
+
+// Uncomment to independently test function via endpoint
+// const decodeAccessToken = (req, res) => {
+//     try{
+//         const token = req.header("Access-Token")
+//         if (!token) throw "No token";
+//         var options = {
+//             algorithms: ["RS256"],
+//             maxAge: "1d",
+//             issuer: "Wechews",
+//             audience: "https://wechews.herokuapp.com"
+//         }
+//         var payload = jwt.verify(token, publicKey, options);
+//         // consider checking db to see if uid exists
+//         req.uid = payload.uid
+//         return res.status(200).send(payload.uid);
+//     } catch (error) {
+//         return res.status(500).send(error.message);
+//     }
+// }
+
+// Generate refresh token
+// Generate upon login or daily app open (further trigger events TBD)
+// Do not expose as indepedent endpoint - attach it to login or other event
+const getRefreshToken = (uid) => {
+    try {
+        var payload = {
+            uid: uid
+        }
+        var options = {
+            algorithm: "HS256", // TODO: Decide on HS256 vs RS256
+            expiresIn: "30d", 
+            issuer: "Wechews",
+            audience: "https://wechews.herokuapp.com"
+        }
+        return jwt.sign(payload, secret, options);
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Uncomment to independently test function via endpoint
+// const getRefreshToken = (req, res) => {
+//     try {
+//         var payload = {
+//             uid: req.body.uid
+//         }
+//         var options = {
+//             algorithm: "HS256",
+//             expiresIn: "30d", 
+//             issuer: "Wechews",
+//             audience: "https://wechews.herokuapp.com"
+//         }
+//         return res.status(200).send(jwt.sign(payload, secret, options));
+//     } catch (error) {
+//         return res.status(500).send(error.message);
+//     }
+// }
+
+// Decodes refresh token and returns uid
+const decodeRefreshToken = (req) => {
+    try{
+        const token = req.header("Refresh-Token");
+        if (!token) throw "Invalid header";
+        var options = {
+            algorithms: ["HS256"],
+            maxAge: "30d",
+            issuer: "Wechews",
+            audience: "https://wechews.herokuapp.com"
+        }
+        var payload = jwt.verify(token, secret, options);
+        // consider checking db to see if uid exists
+        return payload.uid;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Uncomment to independently test function via endpoint
+// const decodeRefreshToken = (req, res) => {
+//     try{
+//         const token = req.header("Refresh-Token")
+//         if (!token) throw "Invalid header";
+//         var options = {
+//             algorithms: ["HS256"],
+//             maxAge: "30d",
+//             issuer: "Wechews",
+//             audience: "https://wechews.herokuapp.com"
+//         }
+//         var payload = jwt.verify(token, secret, options);
+//         // consider checking db to see if uid exists
+//         return res.status(200).send(payload.uid);
+//     } catch (error) {
+//         return res.status(200).send(error.message);
+//     }
+// }
 
 // Middleware: Checks if user has been authenticated
 const isAuthenticated = (req, res, next) => {
@@ -60,7 +154,9 @@ const isAuthenticated = (req, res, next) => {
 // Import these functions into relevant backend files - do not make these directly accessible by api call
 // Store refresh token and access token in async storage -- if refresh token expires, logout
 module.exports = {
-    generateJWT,
-    decodeJWT,
+    getAccessToken,
+    decodeAccessToken,
+    getRefreshToken,
+    decodeRefreshToken,
     isAuthenticated
 }
