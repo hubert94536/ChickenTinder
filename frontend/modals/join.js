a7f9495180810eec8708939874c869684ec117c3
Hubert Chen <30395134+hubert94536@users.noreply.github.com>
2020-11-06 14:04:24 -0800

    WIP Navigation + Home Page (#93)
    * nav, search, home
    
    added nav bar, changed look of search and home page
    
    * navigation bar and some style stuff
    
    * Update App.js
    
    Co-authored-by: Hanna <hannaco@ucla.edu>

--- frontend/modals/join.js
+++ frontend/modals/join.js
@@ -5,17 +5,22 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import PropTypes from 'prop-types'
 import socket from '../apis/socket.js'
 import modalStyles from '../../styles/modalStyles.js'
+import { TextInput } from 'react-native-paper'
+import screenStyles from '../../styles/screenStyles.js'
 
 const hex = '#F25763'
-const font = 'CircularStd-Medium'
+const font = 'CircularStd-Bold'
 //  props are name, image url, and functions for cancel and go
 // invite alert
 
-export default class Invite extends React.Component {
+export default class Join extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       pressed: false,
+      code: '',
+      isValid: false,
+      invalid: false
     }
   }
 
@@ -29,18 +34,19 @@ export default class Invite extends React.Component {
     this.props.cancel()
   }
 
+  evaluatePin(code) {
+    this.setState({code: code, invalid: false})
+    if (code.length === 6)
+      this.setState({isValid: true})
+    else
+      this.setState({isValid: false})
+  }
+
   render() {
     return (
-      <View>
-        <Text />
-        <BlurView
-          blurType="light"
-          blurAmount={20}
-          reducedTransparencyFallbackColor="white"
-          style={modalStyles.blur}
-        />
+      <View style={{position: 'absolute'}}>
         <Modal transparent animationType="none">
-          <View style={modalStyles.modal}>
+          <View style={[modalStyles.modal, {height: 180}]}>
             <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
               <Icon
                 name="times-circle"
@@ -54,30 +60,22 @@ export default class Invite extends React.Component {
                 justifyContent: 'space-evenly',
               }}
             >
-              <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                <Image source={{ uri: this.props.image }} style={styles.avatar} />
-                <View>
-                  <Text
-                    style={{
-                      fontFamily: font,
-                      color: hex,
-                      fontSize: 25,
-                    }}
-                  >
-                    {this.props.name}
-                  </Text>
-                  <Text
-                    style={{
-                      fontFamily: font,
-                      color: hex,
-                      fontSize: 25,
-                    }}
-                  >
-                    invites you to join!
-                  </Text>
-                </View>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems:'center' }}>
+                <Text style={[screenStyles.text, {fontSize: 20}]}>Group PIN:</Text>
+                <TextInput
+                  style={[screenStyles.input], {height: 30, fontSize: 20, textAlignVertical:'center'}}
+                  placeholderTextColor='#999999'
+                  textAlign='left'
+                  placeholder='e.g. A12345'
+                  onChangeText={(code) => {
+                    this.evaluatePin(code)
+                  }}
+                  value={this.state.code}
+              />
               </View>
-              <TouchableHighlight
+              {this.state.invalid && <Text style={{textAlign:'center'}}>Sorry, PIN is invalid or expired</Text>}
+              {this.state.isValid && 
+                <TouchableHighlight
                 underlayColor={hex}
                 onHideUnderlay={() => this.setState({ pressed: false })}
                 onShowUnderlay={() => this.setState({ pressed: true })}
@@ -90,9 +88,22 @@ export default class Invite extends React.Component {
                     this.state.pressed ? { color: 'white' } : { color: hex },
                   ]}
                 >
-                  Go
+                  Join Group
                 </Text>
               </TouchableHighlight>
+            }
+            {!this.state.isValid && 
+                <TouchableHighlight
+                onPress={() => this.setState({invalid:true})}
+                style={[modalStyles.button, {backgroundColor: '#999999', borderColor: '#999999'}]}
+              >
+                <Text
+                  style={[ modalStyles.text, {color: 'white'}]}
+                >
+                  Join Group
+                </Text>
+              </TouchableHighlight>
+            }
             </View>
           </View>
         </Modal>
@@ -101,7 +112,7 @@ export default class Invite extends React.Component {
   }
 }
 
-Invite.propTypes = {
+Join.propTypes = {
   username: PropTypes.string,
   image: PropTypes.string,
   cancel: PropTypes.func,
