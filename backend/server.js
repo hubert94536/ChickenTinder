cc2c3d5d891963e01c7469d1f3a17818c81af527
Hubert Chen <30395134+hubert94536@users.noreply.github.com>
2020-11-16 21:43:39 -0800

    Helentest (#110)
    * Untested schema validation for accounts + friendsQueries
    
    * Remove schema valid fxs for put and delete, add required()
    
    * Edits to updateAccount schema and id (bigint) type check
    
    * Typo edits
    
    * Fixes to POST schema and gen validation fx to use params
    
    * Add route validation
    
    * Add handler to check route for post/put
    
    Co-authored-by: hwang635 <hejwa9@yahoo.com>
    Co-authored-by: Helen Wang <28603722+hwang635@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const io = require('socket.io')()
 const bodyParser = require('body-parser')
 const accounts = require('./accountsQueries.js')
 const friends = require('./friendsQueries.js')
+const Joi = require('joi')
 const http = require('http')
+
+//  For validating param passed through route
+const validateRoute = require('express-joi-validation').createValidator({})
 
 const app = express()
 const server = http.createServer(app)
@@ -18,35 +22,121 @@ app.use(
     extended: true,
   }),
 )
+
 // if development mode, allow self-signed ssl
 if (app.get('env') === 'development') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 }
 
+//General helper fx for validating schema
+function validateRequest(req, next, schema) {
+  const validationOptions = {
+    abortEarly: false, //returns all errors found
+    allowUnknown: true, //allow obj to have unknown key (props)
+    stripUnknown: true, //remove unknown elem from objs/arrays
+  }
+  // schema.validate(val, options) ==> validates val using current schema and options
+  // Returns obj w keys value (validated, normalised val) and error (any validation errors)
+  const { value, error } = schema.validate(req.body.params, validationOptions)
+  if (error) {
+    next(`Validation error: ${error.details.map((x) => x.message).join(', ')}`)
+  } else {
+    req.body.params = value
+    next()
+  }
+}
+
 // accounts table
-app.route('/accounts').get(accounts.getAllAccounts).post(accounts.createAccount)
+app
+  .route('/accounts')
+  .get(accounts.getAllAccounts)
+  .post(checkCreateAccountsSchema, accounts.createAccount)
 
-app.route('/accounts/search/:text').get(accounts.searchAccounts)
+function checkCreateAccountsSchema(req, res, next) {
+  const createAccountsSchema = Joi.object().keys({
+    id: Joi.number().positive().unsafe().required(), //ids are BigInts, which can be outside of the safe range
+    name: Joi.string().required(),
+    username: Joi.string().required(),
+    email: Joi.string().email().required(),
+    photo: Joi.string().required(),
+    phone_number: Joi.string().min(7).max(15),
+  })
+  validateRequest(req, next, createAccountsSchema)
+}
 
+//Check that string text is passed in through the route
+const searchAccountSchema = Joi.object().keys({
+  text: Joi.string().required(),
+})
+app
+  .route('/accounts/search/:text')
+  .get(validateRoute.params(searchAccountSchema), accounts.searchAccounts)
+
+//Check that number id is passed in for GET and DELETE
+const accountByIdSchema = Joi.object().keys({
+  id: Joi.number().positive().unsafe().required(),
+})
 app
   .route('/accounts/:id')
-  .get(accounts.getAccountById)
-  .put(accounts.updateAccount)
-  .delete(accounts.deleteAccount)
+  .get(validateRoute.params(accountByIdSchema), accounts.getAccountById)
+  .put(checkUpdateAccountSchema, validateRoute.params(accountByIdSchema), accounts.updateAccount)
+  .delete(validateRoute.params(accountByIdSchema), accounts.deleteAccount)
 
-app.route('/username/:username').get(accounts.checkUsername)
+//Check that some prop to be updated is passed in
+function checkUpdateAccountSchema(req, res, next) {
+  const updateAccountSchema = Joi.object()
+    .keys({
+      id: Joi.number().positive().unsafe(), //ids are BigInts, which can be outside of the safe range
+      name: Joi.string(),
+      username: Joi.string(),
+      email: Joi.string().email(),
+      photo: Joi.string(),
+      phone_number: Joi.string().min(7).max(15),
+    })
+    //.or(peer1, peer2, ...) ==> at least 1 peer is required, there can be more than 1
+    .or('name', 'username', 'email', 'photo', 'phone_number')
+  validateRequest(req, next, updateAccountSchema)
+}
 
-app.route('/phoneNumber/:phone_number').get(accounts.checkPhoneNumber)
+const usernameSchema = Joi.object().keys({
+  username: Joi.string().required(),
+})
+app.route('/username/:username').get(validateRoute.params(usernameSchema), accounts.checkUsername)
+
+const phoneNumberSchema = Joi.object().keys({
+  phone_number: Joi.string().min(7).max(15).required(),
+})
+app
+  .route('/phoneNumber/:phone_number')
+  .get(validateRoute.params(phoneNumberSchema), accounts.checkPhoneNumber)
 
 // friendships table
-app.route('/friendships').post(friends.createFriends)
+app.route('/friendships').post(checkCreateFriendsSchema, friends.createFriends)
+function checkCreateFriendsSchema(req, res, next) {
+  const createFriendsSchema = Joi.object()
+    .keys({
+      main: Joi.number().integer().positive().unsafe().required(),
+      friend: Joi.number().integer().positive().unsafe().required(),
+    })
+    .with('main', 'friend') //every main must have a friend
+  validateRequest(req, next, createFriendsSchema)
+}
 
-app.route('/friendships/friends/:user').get(friends.getFriends)
+const getFriendsSchema = Joi.object().keys({
+  user: Joi.number().positive().unsafe().required(),
+})
+app
+  .route('/friendships/friends/:user')
+  .get(validateRoute.params(getFriendsSchema), friends.getFriends)
 
+const friendshipSchema = Joi.object().keys({
+  user: Joi.number().positive().unsafe().required(),
+  friend: Joi.number().positive().unsafe().required(),
+})
 app
   .route('/friendships/friends/:user/:friend')
-  .delete(friends.deleteFriendship)
-  .put(friends.acceptRequest)
+  .delete(validateRoute.params(friendshipSchema), friends.deleteFriendship)
+  .put(validateRoute.params(friendshipSchema), friends.acceptRequest)
 
 server.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`)
