94c611d2077b749f09d6f5ee0276dd439e20e657
Kevin Tang <kevintang2023@gmail.com>
2020-12-11 16:08:13 -0800

    New group & filters (#156)
    * Fix merge conflict & some profile fixes
    
    * Pass number of friends from friends.js -> profile.js
    
    * More minor fixes
    
    - TextInput submission now updates name & username
    - Fixed some lint errors
    - Centered more text
    
    * Cleanup
    
    * Update package-lock.json
    
    * Cleanup nav.js
    
    * Filter modifications
    
    * App.js prettier
    
    * Fix prop typos
    
    * Before drawer view
    
    * eslint tweak
    
    * Adjust backgroundButton styling
    
    to better match mockup
    
    * ESLint fixes
    
    * Needlessly complicated draggable component container for filter.js
    
    * Initial testing group.js to support draggable view
    
    * WIP filter.js - still testing
    
    * Clean up filterContainer
    
    * Remove load-in animation
    
    * Create chooseSize modal
    
    * Add preliminary modal support
    
    * Connect chooseFriends.js
    
    * Update tagsView styling and clean up files
    
    * Install npm packages
    
    Installed react-native-slider-text, babel-eslint.
    
    TODO: Uninstall react-native-draggable-view, react-native-slider-text
    
    * Minor styling changes
    
    * Working on FlatList scroll issue
    
    * Fix groupCard collapsing
    
    * Add filters title
    
    * WIP new group
    
    * Fix flatlist not scrolling
    
    * Update App.js
    
    * added logs for backend (#146)
    
    * Hubert fix (#147)
    
    
    * set remote redis
    
    * Super bouncy drawer
    
    * State based drawer
    
    * Offset version
    
    * Fixed case
    
    * Cleanup old component
    
    * Remove unused callbacks
    
    * Remove unused callbacks
    
    * Drawer bounceback
    
    * Fix drawer state bug
    
    * Rename
    
    * Add layering & object height
    
    * CSS offset
    
    * Fix non-interactive zone
    
    * clean console
    
    * Final cleanup
    
    Removed attribution because I don't think there's any similarities anymore
    
    * Rename & fix AnimatedEvent error
    
    * Cleanup & Group PIN
    
    * Rename group.js->Group.js
    
    * Update Drawer.js
    
    * Touchable area fix
    
    * Begin state rework
    
    * Fix object height making drawer non touchable
    
    * remove debugging
    
    * Works (?) needs debugging
    
    * Make sure blur z-index is high enough
    
    * Bugfixes from Thursday
    
    * Cleanup for user mode
    
    * Fix hostName
    
    * Merge from master & renaming
    
    * Location alert crash fix
    
    * Alert default prop
    
    * Revert personal .eslintrc changes
    
    Co-authored-by: Hubert Chen <30395134+hubert94536@users.noreply.github.com>

---
+++ frontend/screens/Drawer.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Animated, PanResponder, Dimensions } from 'react-native'
+import PropTypes from 'prop-types'
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+
+class Drawer extends Component {
+  constructor(props) {
+    super(props)
+
+    const upPos = -1 * this.props.objectHeight
+    const downPos = 0.0 // screen height - thingy size
+
+    this.state = {
+      position: new Animated.Value(0, this.props.initialDrawerPos),
+      closedPosition: upPos,
+      openPosition: downPos,
+      currState: true, // true = top, false = down
+      objectHeight: this.props.objectHeight,
+    }
+
+    this.state.position.setOffset(0)
+    this.state.position.setValue(upPos)
+
+    this._panGesture = PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) => {
+        // console.log('Drawer.js: tryGesture' + gestureState.moveY)
+        return (
+          Math.abs(gestureState.dy) > Math.abs(gestureState.dx) && Math.abs(gestureState.dy) > 1
+        )
+      },
+      onPanResponderGrant: (evt, gestureState) => {
+        if (this.state.currState == true) {
+          this.state.position.setOffset(-this.state.objectHeight)
+        } else {
+          this.state.position.setOffset(0)
+        }
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {
+            dy: this.state.position,
+          },
+        ],
+        { useNativeDriver: false },
+      ),
+      onPanResponderRelease: (evt, gestureState) => {
+        this.state.position.flattenOffset()
+        const goingUp = gestureState.dy < 0 && gestureState.vy < 0
+        const goingDown = gestureState.dy > 0 && gestureState.vy > 0
+        if (goingUp) {
+          this.setState({ currState: true })
+          Animated.spring(this.state.position, {
+            toValue: this.state.closedPosition,
+            useNativeDriver: 'false',
+          }).start()
+        } else if (goingDown) {
+          // console.log('filterContainer.js: goingDown')
+          this.setState({ currState: false })
+          Animated.spring(this.state.position, {
+            toValue: this.state.openPosition,
+            useNativeDriver: 'false',
+          }).start()
+        } else if (!goingUp && !goingDown) {
+          // console.log('filterContainer.js: bounce')
+          Animated.spring(this.state.position, {
+            toValue: this.state.currentState ? this.state.closedPosition : this.state.openPosition,
+            useNativeDriver: 'false',
+          }).start()
+        }
+      },
+    })
+  }
+
+  render() {
+    const containerView = this.props.renderContainerView()
+    const drawerView = this.props.renderDrawerView()
+    const header = this.props.renderHeader()
+
+    return (
+      <View
+        style={[
+          styles.viewport,
+          {
+            top: this.props.offset,
+            height: '100%',
+          },
+        ]}
+      >
+        <View style={{ height: this.props.objectHeight + 40 }} {...this._panGesture.panHandlers}>
+          <Animated.View
+            style={[
+              {
+                translateY: this.state.position.interpolate({
+                  inputRange: [-this.state.objectHeight * 1.1, 0],
+                  outputRange: [-this.state.objectHeight * 1.1, 0],
+                  extrapolate: 'clamp',
+                }),
+                perspective: 1000,
+                height: '100%',
+              },
+              styles.drawer,
+            ]}
+            {...this._panGesture.panHandlers}
+          >
+            {drawerView}
+          </Animated.View>
+        </View>
+        <View style={styles.container}>{containerView}</View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  viewport: {
+    flexDirection: 'column',
+  },
+  drawer: {
+    zIndex: 1,
+    elevation: 1,
+    // display: 'none',
+  },
+  container: {
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    backgroundColor: 'white',
+    zIndex: 0,
+    elevation: 0,
+  },
+})
+
+Drawer.propTypes = {
+  initialDrawerPos: PropTypes.number,
+  finalDrawerPos: PropTypes.number,
+  objectHeight: PropTypes.number,
+  offset: PropTypes.number,
+  onOpen: PropTypes.func,
+  onClose: PropTypes.func,
+  renderContainerView: PropTypes.func,
+  renderDrawerView: PropTypes.func,
+  renderHeader: PropTypes.func,
+}
+
+Drawer.defaultProps = {
+  initialDrawerPos: 0,
+  finalDrawerPos: 0,
+  objectHeight: 600,
+  offset: 0,
+  onOpen: () => {},
+  onClose: () => {},
+  renderContainerView: () => {},
+  renderDrawerView: () => {},
+  renderHeader: () => {},
+}
+
+export default Drawer
---
+++ frontend/screens/Group.js
@@ -0,0 +1,642 @@
+import React from 'react'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from 'react-native'
+import { BlurView } from '@react-native-community/blur'
+import { USERNAME } from 'react-native-dotenv'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import AsyncStorage from '@react-native-community/async-storage'
+import PropTypes from 'prop-types'
+
+import Drawer from './Drawer.js'
+import Alert from '../modals/alert.js'
+import GroupCard from '../cards/groupCard.js'
+import ChooseFriends from '../modals/chooseFriends.js'
+import FilterSelector from './filter.js'
+import socket from '../apis/socket.js'
+import screenStyles from '../../styles/screenStyles.js'
+import modalStyles from '../../styles/modalStyles.js'
+
+const hex = '#F15763'
+
+const font = 'CircularStd-Medium'
+let memberList = []
+let memberRenderList = []
+let myUsername = ''
+AsyncStorage.getItem(USERNAME).then((res) => {
+  myUsername = res
+})
+
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+
+export default class Group extends React.Component {
+  constructor(props) {
+    super(props)
+    this._isMounted = false
+    const members = this.props.navigation.state.params.members
+    this.filterRef = React.createRef()
+    this.state = {
+      myUsername: myUsername,
+
+      // Group data
+      // Member Dictionary
+      members: members,
+
+      host: this.props.navigation.state.params.host,
+      hostName: members[this.props.navigation.state.params.host].username,
+      // hostName: "NOT YOU",
+      needFilters: Object.keys(members).filter((user) => !user.filters).length,
+
+      filters: {},
+      code: this.props.navigation.state.params.code,
+
+      // UI state
+      canStart: false,
+      userSubmitted: false,
+
+      // Modal visibility vars
+      leaveAlert: false,
+      endAlert: false,
+      chooseFriends: false,
+    }
+    this.updateMemberList()
+
+    // listens if user is to be kicked
+    socket.getSocket().on('kick', () => {
+      this.leaveGroup
+    })
+
+    // listens for group updates
+    socket.getSocket().on('update', (res) => {
+      console.log('group.js: Update')
+      if (this._isMounted) {
+        console.log('socket "update": ' + JSON.stringify(res))
+        this.setState({ members: res.members, host: res.host, code: res.code })
+        const count = this.countNeedFilters(res.members)
+        this.setState({ needFilters: count })
+        if (!count) {
+          this.setState({ canStart: true })
+        }
+      }
+    })
+
+    socket.getSocket().on('start', (restaurants) => {
+      // console.log('group.js: ' + JSON.stringify(restaurants))
+      if (restaurants.length > 0) {
+        this.props.navigation.navigate('Round', {
+          results: restaurants,
+          host: this.state.host,
+          isHost: this.state.hostName === this.state.myUsername,
+          code: this.state.code,
+        })
+      } else {
+        console.log('no restaurants found')
+        // need to handle no restaurants returned
+      }
+    })
+
+    socket.getSocket().on('leave', () => {
+      if (this._isMounted) {
+        this.leaveGroup()
+      }
+    })
+
+    socket.getSocket().on('leave', () => {
+      if (this._isMounted) {
+        this.leaveGroup()
+      }
+    })
+
+    socket.getSocket().on('exception', (error) => {
+      if (this._isMounted) {
+        console.log(error)
+      }
+    })
+  }
+
+  setUserSubmit() {
+    this.setState({ userSubmitted: true })
+  }
+
+  // counts number of users who haven't submitted filters
+  countNeedFilters(users) {
+    let count = 0
+    for (const user in users) {
+      if (!users[user].filters) {
+        count++
+      }
+    }
+    return count
+  }
+
+  // pings server to fetch restaurants, start session
+  start() {
+    // this.filterRef.current.setState({ locationAlert: true })
+    this.filterRef.current.startSession()
+  }
+
+  // update user cards in group
+  updateMemberList() {
+    memberList = []
+    memberRenderList = []
+    for (const user in this.state.members) {
+      const a = {}
+      a.name = this.state.members[user].name
+      a.username = this.state.members[user].username
+      a.user = user
+      a.photo = this.state.members[user].photo
+      a.filters = this.state.members[user].filters
+      a.host = this.state.host
+      a.isHost = this.state.hostName == this.state.myUsername
+      a.key = user
+      a.f = false
+      memberList.push(a)
+      memberRenderList.push(a)
+    }
+    const footer = {}
+    footer.f = true
+    memberRenderList.push(footer)
+    // console.log('\n\n\n\n\n\n=========================')
+    // console.log(memberList)
+    // console.log('=========================\n\n\n\n\n\n')
+  }
+
+  leaveGroup() {
+    socket.leaveRoom(this.state.host)
+    this.props.navigation.navigate('Home')
+  }
+
+  endGroup() {
+    socket.endSession()
+  }
+
+  // shows proper alert based on if user is host
+  cancelAlert() {
+    this.state.hostName === this.state.myUsername
+      ? this.setState({ endAlert: false })
+      : this.setState({ leaveAlert: false })
+  }
+
+  // _handleChooseFriendsPress() {
+  //   console.log('added')
+  //   // add code to close choose friend modal
+  // }
+
+  componentDidMount() {
+    console.log('Group - DidMount')
+    console.log('Group.js: nav params ' + JSON.stringify(this.props.navigation.state.params))
+    this.setState({ _isMounted: true })
+  }
+
+  componentWillUnmount() {
+    console.log('Group - WillUnmount')
+    this.setState({ _isMounted: false })
+    // Todo - potentially add leave group?
+  }
+
+  firstName(str) {
+    const first_sp = str.indexOf(' ')
+    return str.substr(0, first_sp)
+  }
+
+  render() {
+    this.updateMemberList()
+    return (
+      <View style={{ backgroundColor: '#FFF' }}>
+        <View style={[styles.top, styles.floating]}>
+          <View
+            style={{
+              alignSelf: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              backgroundColor: hex,
+              height: 120,
+              width: '100%',
+              paddingBottom: 20,
+            }}
+          >
+            <Text style={styles.groupTitle}>
+              {this.state.hostName === this.state.myUsername
+                ? 'Your Group'
+                : `${this.firstName(this.state.members[this.state.host].name)}'s Group`}
+            </Text>
+            <View style={styles.subheader}>
+              <Text style={styles.pinText}>Group PIN: </Text>
+              <Text style={styles.codeText}>{this.state.code + ' '}</Text>
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  width: 15,
+                  height: 15,
+                }}
+                // TODO: add copy to clipboard/share functionality
+              >
+                <Ionicons name="copy-outline" style={styles.icon2} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <Drawer
+          style={styles.drawer}
+          initialDrawerPos={100}
+          renderContainerView={() => (
+            <View style={styles.main}>
+              <View style={[styles.center, { flexDirection: 'row' }]}>
+                <Icon name="user" style={[styles.icon, { color: '#F15763' }]} />
+                <Text
+                  style={{
+                    color: '#F15763',
+                    fontWeight: 'bold',
+                    fontFamily: font,
+                  }}
+                >
+                  {memberList.length}
+                </Text>
+                <Text style={[styles.divider, { color: '#F15763' }]}>|</Text>
+                <Text style={[styles.waiting, { color: '#F15763' }]}>
+                  waiting for {this.state.needFilters} member filters
+                </Text>
+              </View>
+              <FlatList
+                style={[styles.center, { marginTop: 0, height: windowHeight * 0.5 }]}
+                numColumns={2}
+                ListHeaderComponentStyle={{
+                  color: '#F15763',
+                  marginBottom: 10,
+                }}
+                data={memberRenderList}
+                contentContainerStyle={styles. iner}
+                renderItem={({ item }) => {
+                  if (item.f) {
+                    return (
+                      <View>
+                        <TouchableHighlight
+                          style={{
+                            backgroundColor: '#DCDCDC',
+                            borderRadius: 7,
+                            alignSelf: 'center',
+                            width: 170,
+                            height: 35,
+                            padding: 0,
+                            margin: 5,
+                          }}
+                          onPress={() =>
+                            this.setState({ chooseFriends: true }, () =>
+                              console.log('group.js: chooseFriends = ' + this.state.chooseFriends),
+                            )
+                          }
+                        >
+                          <Text
+                            style={{
+                              color: 'black',
+                              textAlign: 'center',
+                              width: '100%',
+                              lineHeight: 36,
+                            }}
+                          >
+                            + Add Friends
+                          </Text>
+                        </TouchableHighlight>
+                        <View
+                          style={{
+                            width: 170,
+                            height: 30,
+                            padding: 0,
+                            margin: 5,
+                            display: 'none',
+                          }}
+                        >
+                          <Text>footer</Text>
+                        </View>
+                      </View>
+                    )
+                  } else {
+                    return (
+                      <GroupCard
+                        name={item.name}
+                        username={item.username}
+                        image={item.photo}
+                        filters={item.filters}
+                        host={this.state.host}
+                        isHost={this.state.hostName == item.username}
+                        key={item.key}
+                        style={{ width: 170 }}
+                      />
+                    )
+                  }
+                }}
+                keyExtractor={(item, index) => index}
+              />
+              <View style={styles.bottom}>
+                <Text style={styles.bottomText}>
+                  When everyone has submitted filters, the round will begin!
+                </Text>
+                {this.state.hostName === this.state.myUsername && (
+                  <TouchableHighlight
+                    underlayColor="#F15763"
+                    activeOpacity={1}
+                    onPress={() => this.start()}
+                    style={[
+                      screenStyles.bigButton,
+                      styles.bigButton,
+                      this.state.canStart ? { opacity: 0.75 } : { opacity: 1 },
+                    ]}
+                  >
+                    {/* TODO: Change text if required options have not been set */}
+                    <Text style={styles.buttonText}>Start Round</Text>
+                  </TouchableHighlight>
+                )}
+                {this.state.hostName !== this.state.myUsername && (
+                  <TouchableHighlight
+                    style={[
+                      screenStyles.bigButton,
+                      styles.bigButton,
+                      !this.state.userSubmitted ? { opacity: 0.75 } : { opacity: 1 },
+                    ]}
+                    onPress={() => {
+                      this.filterRef.current.submitUserFilters()
+                    }}
+                  >
+                    <Text style={styles.buttonText}>
+                      {!this.state.userSubmitted ? 'Submit Filters' : 'Waiting...'}
+                    </Text>
+                  </TouchableHighlight>
+                )}
+                <TouchableHighlight
+                  onShowUnderlay={() => this.setState({ leaveGroup: true })}
+                  onHideUnderlay={() => this.setState({ leaveGroup: false })}
+                  style={styles.leave}
+                  onPress={() => {
+                    console.log(this.state.members)
+                    this.state.hostName === this.state.myUsername
+                      ? this.setState({ endAlert: true })
+                      : this.setState({ leaveAlert: true })
+                  }}
+                  underlayColor="white"
+                >
+                  <Text
+                    style={[
+                      styles.leaveText,
+                      this.state.leaveGroup ? { color: hex } : { color: '#6A6A6A' },
+                    ]}
+                  >
+                    {this.state.hostName === this.state.myUsername ? 'Cancel Group' : 'Leave Group'}
+                  </Text>
+                </TouchableHighlight>
+              </View>
+              {this.state.leaveAlert && (
+                <Alert
+                  title="Leave?"
+                  body="You will will not be able to return without an invite"
+                  buttonAff="Yes"
+                  height="20%"
+                  press={() => this.leaveGroup()}
+                  cancel={() => this.cancelAlert()}
+                />
+              )}
+              {this.state.endAlert && (
+                <Alert
+                  title="End the session?"
+                  body="You will not be able to return"
+                  buttonAff="Yes"
+                  height="20%"
+                  press={() => this.leaveGroup()}
+                  cancel={() => this.cancelAlert()}
+                />
+              )}
+              {this.state.chooseFriends && (
+                <BlurView
+                  blurType="dark"
+                  blurAmount={10}
+                  reducedTransparencyFallbackColor="white"
+                  style={modalStyles.blur}
+                />
+              )}
+              <ChooseFriends
+                visible={this.state.chooseFriends}
+                members={memberList}
+                press={() => this.setState({ chooseFriends: false })}
+              />
+            </View>
+          )}
+          objectHeight={this.state.hostName == this.state.myUsername ? 400 : 400}
+          offset={120}
+          renderDrawerView={() => (
+            <View>
+              <View>
+                <View
+                  style={{
+                    width: windowWidth,
+                    height: 400,
+                    zIndex: 3,
+                    borderColor: '#F15763',
+                    borderWidth: 1,
+                    overflow: 'hidden',
+                  }}
+                >
+                  <FilterSelector
+                    host={this.state.host}
+                    isHost={this.state.hostName == this.state.myUsername}
+                    handleUpdate={() => this.setUserSubmit()}
+                    members={memberList}
+                    ref={this.filterRef}
+                    code={this.state.code}
+                  />
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  margin: '4%',
+                  justifyContent: 'center',
+                }}
+              >
+              <View
+                  style={{
+                    color: 'white',
+                    fontFamily: font,
+                    marginRight: '3%',
+                    height: 70,
+                    backgroundColor: 'white',
+                    padding: 15,
+                    marginTop: -45,
+                    borderRadius: 15,
+                    borderColor: '#F15763',
+                    borderWidth: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'flex-end',
+                    zIndex: 2,
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: '#F15763',
+                      fontFamily: font,
+                      fontSize: 11,
+                    }}
+                  >
+                    {this.state.myUsername === this.state.hostName
+                      ? 'Pull down for host menu'
+                      : 'Pull down to set filters'}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    )
+  }
+}
+
+Group.propTypes = {
+  members: PropTypes.array,
+  host: PropTypes.string,
+}
+
+const styles = StyleSheet.create({
+  // Containerse
+  main: {
+    marginTop: 35,
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: 'white',
+    color: '#aaa',
+  },
+  groupTitle: {
+    color: '#fff',
+    fontSize: 30,
+    marginTop: '7%',
+    fontWeight: 'bold',
+    fontFamily: font,
+    alignSelf: 'center',
+  },
+  leave: {
+    alignSelf: 'center',
+    marginTop: '3%',
+    borderRadius: 25,
+    width: '25%',
+  },
+  leaveText: {
+    fontFamily: font,
+    textAlign: 'center',
+    fontSize: 16,
+    paddingTop: '2%',
+    paddingBottom: '2%',
+  },
+  icon: {
+    color: '#aaa',
+    marginLeft: '5%',
+    marginTop: '2%',
+    fontSize: 30,
+  },
+  divider: {
+    color: '#aaa',
+    alignSelf: 'center',
+    marginLeft: '3%',
+    fontSize: 25,
+    fontFamily: font,
+  },
+  waiting: {
+    color: '#aaa',
+    marginLeft: '3%',
+    alignSelf: 'center',
+    fontFamily: font,
+  },
+  button: {
+    borderRadius: 25,
+    borderWidth: 2.5,
+    borderColor: '#aaa',
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    width: '30%',
+    alignSelf: 'center',
+    alignContent: 'center',
+    marginTop: '3%',
+  },
+  buttonText: {
+    color: '#fff',
+    alignSelf: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bottomText: {
+    color: '#aaa',
+    width: '50%',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: font,
+  },
+  bigButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 4,
+    width: '50%',
+    marginTop: '3%',
+    backgroundColor: '#F15763',
+  },
+  top: {
+    backgroundColor: '#fff',
+    top: 0,
+  },
+  center: {
+    flex: 0.6,
+    margin: 15,
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  bottom: {
+    flex: 0.5,
+    color: '#aaa',
+  },
+  memberContainer: {
+    width: '100%',
+  },
+  subheader: {
+    color: '#FFF',
+    marginTop: 5,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  pinText: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'normal',
+    fontFamily: font,
+    alignSelf: 'center',
+  },
+  codeText: {
+    color: '#ffffff',
+    fontFamily: font,
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  icon2: {
+    color: '#fff',
+    fontSize: 15,
+    marginLeft: '7%',
+  },
+  drawer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  floating: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 20,
+    elevation: 20,
+  },
+})
