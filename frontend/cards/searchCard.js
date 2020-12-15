fd4147c8a10f635e429294cec8db3ba257ffd979
Francis Feng <francisfeng0@gmail.com>
2020-12-15 14:44:36 -0800

    Rename from Southern_SoulFood.png to Southern_Soulfood.png

--- frontend/cards/searchCard.js
+++ frontend/cards/searchCard.js
@@ -68,10 +68,6 @@ export default class SearchCard extends React.Component {
   }
 
   render() {
-    console.log('prop showError: ' + this.props.showError)
-    console.log('prop showDelete: ' + this.props.showDelete)
-    console.log('prop deleteError: ' + this.props.deleteError)
-
     return (
       <View style={{ flexDirection: 'row', flex: 1, width: '85%', alignSelf: 'center' }}>
         <Image
