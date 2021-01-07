fce55a171ee187b285467f0649961ff5ecc538cc
Francis Feng <francisfeng0@gmail.com>
2021-01-07 15:56:44 -0800

    Fix Styles for ProfileSettings
    style: fix color styles for ProfileSettings

--- frontend/modals/ProfileSettings.js
+++ frontend/modals/ProfileSettings.js
@@ -62,7 +62,7 @@ export default class Settings extends React.Component {
           </View>
           <View style={styles.bodyContainer}>
             <View>
-              <Text style={[styles.subTitle, screenStyles.text]}>Email</Text>
+              <Text style={[screenStyles.text, styles.subTitle]}>Email</Text>
               <TextInput
                 style={[screenStyles.text, screenStyles.input, styles.textInput]}
                 editable={false}
@@ -71,7 +71,7 @@ export default class Settings extends React.Component {
             </View>
           </View>
           <View style={[styles.bodyContainer, styles.bodyMargin]}>
-            <Text style={[styles.subTitle, screenStyles.text]}>
+            <Text style={[screenStyles.text, styles.subTitle]}>
               Phone Number
             </Text>
             <TextInput
