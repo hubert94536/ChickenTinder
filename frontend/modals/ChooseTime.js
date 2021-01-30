cb83f02faa29a02676770a8cb8237b09a2d6170b
Kevin Tang <kevintang2023@gmail.com>
2021-01-30 10:42:55 -0800

    Working ZIP code, map, & radius selector (#254)
    feat: add map feature to show location with radius selector
    fix: group bug fixes

--- frontend/modals/ChooseTime.js
+++ frontend/modals/ChooseTime.js
@@ -15,7 +15,7 @@ import modalStyles from '../../styles/modalStyles.js'
 import normalize from '../../styles/normalize.js'
 import screenStyles from '../../styles/screenStyles.js'
 import Icon from 'react-native-vector-icons/AntDesign'
-import SwitchButton from 'switch-button-react-native'
+import TimeSwitch from './TimeSwitch.js'
 
 export default class Time extends React.Component {
   constructor(props) {
@@ -92,34 +92,7 @@ export default class Time extends React.Component {
                 keyboardType="numeric"
               />
               <View style={styles.switchButton}>
-                {/* <Switch
-                  value={this.state.switch}
-                  onValueChange={(val) =>
-                    this.setState({ timeMode: val, switch: !this.state.switch })
-                  }
-                  disabled={false}
-                  activeText={''}
-                  inActiveText={''}
-                  circleSize={30}
-                  barHeight={30}
-                  circleBorderWidth={0}
-                  backgroundActive={'grey'}
-                  backgroundInactive={'grey'}
-                  circleActiveColor={colors.hex}
-                  circleInActiveColor={colors.hex}
-                  changeValueImmediately={true}
-                  renderInsideCircle={() => <Text>{this.state.timeMode}</Text>} // custom component to render inside the Switch circle (Text, Image, etc.)
-                  changeValueImmediately={true} // if rendering inside circle, change state immediately or wait for animation to complete
-                  innerCircleStyle={styles.switchButtonInner} // style for inner animated circle for what you (may) be rendering inside the circle
-                  outerCircleStyle={styles.switchButtonOuter} // style for outer animated circle
-                  renderActiveText={true}
-                  renderInActiveText={true}
-                  switchLeftPx={2} // denominator for logic when sliding to TRUE position. Higher number = more space from RIGHT of the circle to END of the slider
-                  switchRightPx={2} // denominator for logic when sliding to FALSE position. Higher number = more space from LEFT of the circle to BEGINNING of the slider
-                  switchWidthMultiplier={1.5} // multipled by the `circleSize` prop to calculate total width of the Switch
-                  switchBorderRadius={30} // Sets the border Radius of the switch slider. If unset, it remains the circleSize.
-                /> */}
-                <SwitchButton
+                <TimeSwitch
                   onValueChange={(val) => this.setState({ timeMode: val })}
                   text1="pm"
                   text2="am"
