809b054642e8479420ee1047caae9b043ead5624
Isha Gonugunta <56519324+ishagonu@users.noreply.github.com>
2021-01-31 23:58:56 -0800

    Create Account fields fixed (#271)

--- frontend/screens/CreateAccount.js
+++ frontend/screens/CreateAccount.js
@@ -31,6 +31,8 @@ class createAccount extends React.Component {
       validEmail: true,
       validEmailFormat: true,
       validUsername: true,
+      validUsernameFormat: true,
+      facebook: false
     }
     AsyncStorage.multiGet([EMAIL, NAME, PHONE])
       .then((res) => {
@@ -38,12 +40,22 @@ class createAccount extends React.Component {
           email: res[0][1],
           name: res[1][1],
           phone: res[2][1],
-        })
+          
+      }, () => {
+          if(this.state.email)
+          {
+            this.setState({
+              facebook: true,
+            })
+          }
+      });
       })
       .catch(() => {
         this.setState({ errorAlert: true })
       })
   }
+
+  
 
   //  checks whether or not the username can be set
   handleClick() {
@@ -111,6 +123,18 @@ class createAccount extends React.Component {
     }
   }
 
+  checkUsernameSyntax() {
+    
+    const regex = /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){0,13}[a-zA-Z0-9]$/
+    if (!regex.test(this.state.username)) {
+      this.setState({ validUsernameFormat: false })
+    } else {
+      this.setState({ validUsernameFormat: true })
+    }
+  }
+
+ 
+
   render() {
     return (
       <View style={[screenStyles.mainContainer]}>
@@ -118,14 +142,14 @@ class createAccount extends React.Component {
           Create Account
         </Text>
         <Text style={[screenStyles.textBook, styles.mediumText]}>Account Verified!</Text>
-        <Text style={[screenStyles.textBook, styles.mediumText]}>
+        <Text style={[screenStyles.textBook, styles.mediumText, styles.instr]}>
           Finish setting up your account
         </Text>
         <Image
           source={{ uri: Image.resolveAssetSource(this.state.photo).uri }}
-          style={screenStyles.avatar}
+          style={styles.avatar}
         />
-        <Text style={[screenStyles.textBook, styles.fieldName, { marginTop: '5%' }]}>
+        <Text style={[screenStyles.textBook, styles.fieldName, styles.display]}>
           Display Name
         </Text>
         <TextInput
@@ -143,11 +167,12 @@ class createAccount extends React.Component {
           style={[
             screenStyles.textBook,
             styles.fieldText,
-            { marginBottom: this.state.validUsername ? '3%' : '0%' },
+            { marginBottom: this.state.validUsername && this.state.validUsernameFormat ? '7%' : '0%' },
           ]}
           textAlign="left"
           onChangeText={(username) => {
             this.setState({ username: username.split(' ').join('_') })
+            this.checkUsernameSyntax()
           }}
           onBlur={() => this.checkUsernameValidity()}
           value={this.state.username}
@@ -157,38 +182,51 @@ class createAccount extends React.Component {
         {!this.state.validUsername && (
           <Text style={[screenStyles.text, styles.warningText]}>This username is taken</Text>
         )}
+        {!this.state.validUsernameFormat && (
+          <Text style={[screenStyles.text, styles.warningText]}>Invalid username format</Text>
+        )}
 
-        <Text style={[screenStyles.textBook, styles.fieldName]}>Phone Number</Text>
-        <TextInput
-          style={[screenStyles.textBook, styles.fieldText]}
-          textAlign="left"
-          onChangeText={(phone) => {
-            this.setState({ phone })
-          }}
-          value={this.state.phone}
-        />
+        
 
-        <Text style={[screenStyles.textBook, styles.fieldName]}>Email</Text>
-        <TextInput
+        
+
+        {!this.state.facebook && (
+
+          <View>
+
+          <Text style={[screenStyles.textBook, styles.fieldName]}>Phone Number</Text>
+          
+          <Text
           style={[
             screenStyles.textBook,
             styles.fieldText,
-            { marginBottom: this.state.validEmail && this.state.validEmailFormat ? '3%' : '0%' },
+            styles.fixedText,
           ]}
           textAlign="left"
-          onChangeText={(email) => {
-            this.setState({ email: email })
-          }}
-          onBlur={() => this.checkEmailValidity()}
-          value={this.state.email}
-        />
+          >{this.state.phone}</Text>
 
-        {!this.state.validEmail && this.state.validEmailFormat && (
-          <Text style={[screenStyles.text, styles.warningText]}>This email is taken</Text>
+          </View>
+        
         )}
-        {!this.state.validEmailFormat && (
-          <Text style={[screenStyles.text, styles.warningText]}>Input a valid email</Text>
+        
+        {this.state.facebook && (
+
+          <View>
+        <Text style={[screenStyles.textBook, styles.fieldName]}>Email</Text>
+        
+        <Text
+        style={[
+          screenStyles.textBook,
+          styles.fieldText,
+          styles.fixedText,
+        ]}
+        textAlign="left"
+        >{this.state.email}</Text>
+
+        </View>
         )}
+        
+
 
         <TouchableHighlight
           onShowUnderlay={() => this.setState({ finishPressed: true })}
@@ -243,16 +281,30 @@ createAccount.propTypes = {
   changeImagee: PropTypes.func,
 }
 const styles = StyleSheet.create({
+  display: { 
+    marginTop: '5%' 
+  },
+  instr: { 
+    marginBottom: '5%' 
+  },
   title: {
     fontSize: normalize(25),
     marginTop: '10%',
     marginBottom: '5%',
   },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 94.5,
+    borderWidth: 4,
+    alignSelf: 'center',
+    margin: '1.5%',
+  },
   button: {
     borderColor: hex,
     backgroundColor: hex,
-    width: '20%',
-    marginTop: '3%',
+    // width: '20%',
+    marginTop: '7%',
   },
   mediumText: {
     alignSelf: 'center',
@@ -263,10 +315,15 @@ const styles = StyleSheet.create({
     fontSize: normalize(18),
     color: textColor,
     marginHorizontal: '12%',
-    marginBottom: '3%',
+    marginBottom: '7%',
     paddingVertical: '1%',
     borderBottomColor: textColor,
     borderBottomWidth: 1,
+  },
+  fixedText: {
+    paddingVertical: '2%', 
+    paddingHorizontal: '2%',
+
   },
   fieldName: {
     fontSize: normalize(18.5),
