396847b4eace5fb0361aa65e71ecc5ddb956f69a
Hubert Chen <30395134+hubert94536@users.noreply.github.com>
2021-02-07 16:22:13 -0800

    Bug Fixes (#290)
    Fix: App.js connects only once now
    Fix: Update notifications api and socket invite
    Fix: Remove unnecessary socket.off()
    Fix: Switch navigate to replace
    Fix: create group works properly after creating account

--- frontend/screens/Group.js
+++ frontend/screens/Group.js
@@ -87,7 +87,6 @@ export default class Group extends React.Component {
 
     socket.getSocket().once('start', (restaurants) => {
       if (restaurants.length > 0) {
-        socket.getSocket().off()
         global.restaurants = restaurants
         this.props.navigation.replace('Round')
       } else {
@@ -96,19 +95,16 @@ export default class Group extends React.Component {
       }
     })
 
-    socket.getSocket().once('leave', () => {
+    socket.getSocket().on('leave', () => {
       this.leaveGroup(true)
     })
 
-    socket.getSocket().once('reselect', () => {
+    socket.getSocket().on('reselect', () => {
       console.log('reselect')
     })
 
-    socket.getSocket().once('reselect', () => {
-      console.log('reselect')
-    })
   }
-
+  
   setUserSubmit() {
     this.setState({ userSubmitted: true })
   }
@@ -244,7 +240,10 @@ export default class Group extends React.Component {
                 </Text>
                 <Text style={styles.divider}>|</Text>
                 <Text style={styles.waiting}>
-                  waiting for {this.countNeedFilters(this.state.members)} member filters
+                  {this.countNeedFilters(this.state.members) == 0
+                    ? 'waiting for host to start'
+                    : `waiting for ${this.countNeedFilters(this.state.members)} member filters`
+                  }
                 </Text>
               </View>
               <FlatList
