53e2f0a762e1a712b3657d3266f4f620445ab9c7
Kevin Tang <kevintang2023@gmail.com>
2021-02-10 20:24:07 -0800

    Working new modal

--- frontend/modals/ChooseLocation.js
+++ frontend/modals/ChooseLocation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {
+  PermissionsAndroid,
   Dimensions,
   StyleSheet,
   Text,
@@ -8,6 +9,7 @@ import {
   Modal,
   TextInput,
 } from 'react-native'
+import Geolocation from 'react-native-geolocation-service'
 import PropTypes from 'prop-types'
 import MapView, { Circle, Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import modalStyles from '../../styles/modalStyles.js'
@@ -32,6 +34,27 @@ let clientBuilder = new SmartyStreetsCore.ClientBuilder(
 )
 let client = clientBuilder.buildUsZipcodeClient()
 
+const requestLocationPermission = async () => {
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+    title: 'Location Permission',
+    message: 'WeChews needs access to your location ' + 'so you can find nearby restaurants.',
+    buttonNeutral: 'Ask Me Later',
+    buttonNegative: 'Cancel',
+    buttonPositive: 'OK',
+  })
+    .then((res) => {
+      if (res === PermissionsAndroid.RESULTS.GRANTED) {
+        return true
+      } else {
+        return false
+      }
+    })
+    .catch(() => {
+      this.setState({ errorAlert: true })
+      this.props.setBlur(true)
+    })
+}
+
 export default class Location extends Component {
   constructor(props) {
     super(props)
@@ -42,12 +65,11 @@ export default class Location extends Component {
         latitude: 34.06892,
         longitude: -118.445183,
       },
-      city: null,
+      city: 'Choose a location',
+      state: '',
       distance: 5.5,
       zip: '',
       zipValid: true,
-      //style of the button
-      buttonPressed: false,
     }
   }
 
@@ -59,14 +81,16 @@ export default class Location extends Component {
     client
       .send(lookup)
       .then((res) => {
-        console.log(JSON.stringify(res.lookups[0].result[0]))
+        // console.log(JSON.stringify(res.lookups[0].result[0]))
         let result = res.lookups[0].result[0]
         if (result.valid) {
           // console.log('valid zip: ' + this.state.zip.toString())
           let data = result.zipcodes[0]
+          console.log(JSON.stringify(data))
           this.setState({
             zipValid: true,
-            city: result.city,
+            city: data.defaultCity,
+            state: ', ' + data.stateAbbreviation,
             location: {
               latitude: data.latitude,
               longitude: data.longitude,
@@ -89,14 +113,47 @@ export default class Location extends Component {
         this.setState({ zipValid: false })
       })
   }
+
   // function called when main button is pressed w/ valid ZIP
   handlePress() {
-    if (this.state.zipValid) this.props.update(this.state.distance, this.state.location)
+    console.log(this.state.city)
+    // if (this.state.zipValid) this.props.update(this.state.distance, this.state.location)
   }
 
   //  function called when 'x' is pressed
   handleCancel() {
     this.props.cancel()
+  }
+
+  getLocation() {
+    if (requestLocationPermission()) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
+            zipValid: true,
+            city: 'Current Location',
+            state: '',
+            location: {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            },
+          })
+          let newRegion = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: this.state.distance / 60 + 0.3,
+            longitudeDelta: this.state.distance / 60 + 0.3,
+          }
+          this.mapView.current.animateToRegion(newRegion, 3000)
+        },
+        (error) => {
+          console.log(error.code, error.message)
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+      )
+    } else {
+      console.log('Filter.js: Failed to get location')
+    }
   }
 
   render() {
@@ -136,35 +193,101 @@ export default class Location extends Component {
               left: '7%',
               right: '7%',
               alignSelf: 'center',
-              height: '20%',
+              height: '30%',
               borderRadius: 7,
-              backgroundColor: 'green',
+              backgroundColor: 'white',
+
+              flexDirection: 'column',
+              justifyContent: 'flex-start',
             }}
-          ></View>
-          <Slider
-            style={styles.sliderStyle}
-            minimumValue={5}
-            maximumValue={25}
-            value={5}
-            step={0.25}
-            minimumTrackTintColor={colors.hex}
-            maximumTrackTintColor={colors.hex}
-            thumbTintColor={colors.hex}
-            onValueChange={(value) => {
-              this.setState({ distance: value })
-              let newRegion = {
-                latitude: this.state.location.latitude,
-                longitude: this.state.location.longitude,
-                latitudeDelta: value / 40 + 0.2,
-                longitudeDelta: value / 40 + 0.2,
-              }
-              this.mapView.current.animateToRegion(newRegion, 100)
-            }}
-          />
+          >
+            {/* Top - title & expand/contract */}
+            <View
+              style={{
+                flex: 0.5,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={[styles.title, screenStyles.textBook, screenStyles.hex]}>
+                {this.state.city + this.state.state}
+              </Text>
+            </View>
+            {/* Horizontal Line */}
+            <View
+              style={{
+                height: 1,
+                width: '100%',
+                borderWidth: 1,
+                borderColor: '#E0E0E0',
+              }}
+            />
+            {/* Middle - zip code text input, current location button */}
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <TextInput
+                style={[screenStyles.input, screenStyles.textBook, styles.input]}
+                keyboardType="number-pad"
+                placeholderTextColor="#9F9F9F"
+                placeholder="Enter your zip code"
+                textAlign="center"
+                onSubmitEditing={({ nativeEvent }) => {
+                  this.setState({ zip: nativeEvent.text }, () => this.validateZip())
+                }}
+              />
+              <TouchableHighlight
+                underlayColor="white"
+                onPress={() => this.getLocation()}
+                style={[modalStyles.button, styles.buttonColor, styles.locationButton]}
+              >
+                <Text style={[modalStyles.text, styles.white, styles.locationText]}>
+                  Get Current Location
+                </Text>
+              </TouchableHighlight>
+            </View>
+            {/* Horizontal Line */}
+            <View
+              style={{
+                height: 1,
+                width: '100%',
+                borderWidth: 1,
+                borderColor: '#E0E0E0',
+              }}
+            />
+            {/* Bottom - slider & info text */}
+            <View>
+              <Slider
+                style={styles.sliderStyle}
+                minimumValue={5}
+                maximumValue={25}
+                value={5}
+                step={0.25}
+                minimumTrackTintColor={colors.hex}
+                maximumTrackTintColor={colors.hex}
+                thumbTintColor={colors.hex}
+                onValueChange={(value) => {
+                  this.setState({ distance: value })
+                  let newRegion = {
+                    latitude: this.state.location.latitude,
+                    longitude: this.state.location.longitude,
+                    latitudeDelta: value / 40 + 0.2,
+                    longitudeDelta: value / 40 + 0.2,
+                  }
+                  this.mapView.current.animateToRegion(newRegion, 100)
+                }}
+              />
+            </View>
+          </View>
+
           <TouchableHighlight
             underlayColor="white"
-            onHideUnderlay={() => this.setState({ buttonPressed: false })}
-            onShowUnderlay={() => this.setState({ buttonPressed: true })}
             onPress={() => this.handlePress()}
             style={[modalStyles.button, styles.buttonColor, styles.doneButton]}
           >
@@ -185,13 +308,13 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderRadius: 14,
     overflow: 'hidden',
-    marginTop: fullHeight * 0.15,
-    marginBottom: fullHeight * 0.15,
-    height: fullHeight * 0.7,
+    marginTop: fullHeight * 0.11,
+    marginBottom: fullHeight * 0.11,
+    height: fullHeight * 0.78,
     width: fullWidth * 0.8,
   },
   map: {
-    height: fullHeight * 0.7,
+    height: fullHeight * 0.78,
     width: fullWidth * 0.8,
   },
   buttonColor: {
@@ -199,13 +322,25 @@ const styles = StyleSheet.create({
   },
   white: {
     color: 'white',
+    fontWeight: '300',
   },
   sliderStyle: {
     width: '85%',
     height: 30,
     alignSelf: 'center',
-    position: 'absolute',
-    bottom: fullHeight * 0.1,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: normalize(20),
+  },
+  input: {
+    color: 'black',
+    fontSize: normalize(14),
+    fontWeight: '300',
+    backgroundColor: '#E0E0E0',
+    width: '85%',
+    alignSelf: 'center',
+    borderRadius: 5,
   },
   distanceText: {
     color: '#747474',
@@ -216,6 +351,20 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     bottom: 15,
     height: normalize(25),
+  },
+  locationButton: {
+    borderRadius: 5,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    flexDirection: 'column',
+    fontSize: normalize(14),
+    width: '85%',
+    height: '25%',
+  },
+  locationText: {
+    fontSize: normalize(14),
+    fontWeight: '300',
   },
 })
 
