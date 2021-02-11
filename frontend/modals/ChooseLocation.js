56900523057375c594ad1ee155f801718fc8fbb2
Kevin Tang <kevintang2023@gmail.com>
2021-02-10 21:29:19 -0800

    Close modal

--- frontend/modals/ChooseLocation.js
+++ frontend/modals/ChooseLocation.js
@@ -116,8 +116,7 @@ export default class Location extends Component {
 
   // function called when main button is pressed w/ valid ZIP
   handlePress() {
-    console.log(this.state.city)
-    // if (this.state.zipValid) this.props.update(this.state.distance, this.state.location)
+    this.props.update(this.state.distance, this.state.location)
   }
 
   //  function called when 'x' is pressed
