1183f85bf5e4a7e6e8f5cf3980db5559d21906d3
Hubert Chen <hubert94536@yahoo.com>
2021-03-17 22:25:53 -0700

    ssl

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,7 @@ require('./socketEvents.js')(io)
 
 var PORT = process.env.PORT || 5000
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-)
+app.use(express.json())
 
 app.set('etag', false)
 
