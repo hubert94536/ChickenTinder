7f10dc56472a8f4cfaeb0e76b54a08538297180c
Francis Feng <francisfeng0@gmail.com>
2021-03-25 20:18:53 -0700

    WIP TimeSwitch

--- frontend/modals/ChooseTime.js
+++ frontend/modals/ChooseTime.js
@@ -97,16 +97,6 @@ export default class Time extends React.Component {
               <View style={styles.switchButton}>
                 <TimeSwitch
                   onValueChange={(val) => this.setState({ timeMode: val })}
-                  text1="AM"
-                  text2="PM"
-                  switchWidth={90}
-                  switchHeight={30}
-                  switchBorderColor={screenStyles.hex.color}
-                  btnBorderColor={screenStyles.hex.color}
-                  btnBackgroundColor={screenStyles.hex.color}
-                  fontColor={screenStyles.hex.color}
-                  activeFontColor="white"
-                  style={styles.switchButton}
                 />
               </View>
             </View>
